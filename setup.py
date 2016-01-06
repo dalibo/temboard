e2972f554f9d3b1daeb647b08cc8498e97e5851e
Julien Tachoires <julmon@gmail.com>
2016-01-06 17:33:45 +0100

    [install] Add missing packages in setup.py.

--- setup.py
+++ setup.py
@@ -7,8 +7,17 @@ setup(
     version = '0.0.1',
     author = 'Julien Tachoires',
     license = 'PostgreSQL',
-    packages = ['ganeshwebui', 'ganeshwebui.handlers', 'ganeshwebui.plugins.dashboard', 'ganeshwebui.plugins.supervision', 'ganeshwebui.plugins.settings', 'ganeshwebui.plugins.activity'],
-    scripts = ['ganesh-web-client'],
+    packages = [
+            'ganeshwebui',
+            'ganeshwebui.handlers',
+            'ganeshwebui.handlers.manage',
+            'ganeshwebui.model',
+            'ganeshwebui.plugins.dashboard',
+            'ganeshwebui.plugins.supervision',
+            'ganeshwebui.plugins.supervision.model',
+            'ganeshwebui.plugins.settings',
+            'ganeshwebui.plugins.activity'],
+    scripts = ['ganesh-web-client', 'ganeshwebui/plugins/supervision/metric-aggregator'],
     include_package_data=True,
     zip_safe=False,
     url = '',
@@ -18,6 +27,7 @@ setup(
             'share/ssl/ganesh_CHANGEME.pem',
             'share/ssl/ganesh_CHANGEME.key',
             'share/ssl/ganesh_ca_certs_CHANGEME.pem',
-            'share/sql/application.sql']
+            'share/sql/application.sql',
+            'ganeshwebui/plugins/supervision/sql/supervision.sql']
     )]
 )
