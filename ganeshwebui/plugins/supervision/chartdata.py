21106ea20433f380b33b4228c53086393565b9a3
Julien Tachoires <julmon@gmail.com>
2016-01-08 13:54:43 +0100

    [supervision] Performances improvements.
    In SQL queries in charge of getting data for the charts: 'GROUP BY 1'
    and 'ORDER BY 1' have been replaced by 'GROUP BY datetime' and
    'ORDER BY datetime' because '1' is refering to: to_char(datetime, 'YYYY/MM/DD HH24:MI:SS')

--- ganeshwebui/plugins/supervision/chartdata.py
+++ ganeshwebui/plugins/supervision/chartdata.py
@@ -29,7 +29,7 @@ def get_loadaverage(session, hostname, start, end):
     else:
         tablename = 'metric_loadavg_4h'
 
-    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, load1, load5, load15 FROM supervision.%s WHERE hostname = '%s' AND datetime >= '%s' AND datetime <= '%s' ORDER BY 1) TO STDOUT WITH CSV HEADER"
+    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, load1, load5, load15 FROM supervision.%s WHERE hostname = '%s' AND datetime >= '%s' AND datetime <= '%s' ORDER BY datetime) TO STDOUT WITH CSV HEADER"
 
     cur = session.connection().connection.cursor()
     cur.copy_expert(query % (tablename, hostname, start.strftime('%Y-%m-%dT%H:%M:%S'), end.strftime('%Y-%m-%dT%H:%M:%S')), data_buffer)
@@ -50,7 +50,7 @@ def get_cpu(session, hostname, start, end):
     else:
         tablename = 'metric_cpu_4h'
 
-    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, round((SUM(time_user)/(SUM(time_user)+SUM(time_system)+SUM(time_idle)+SUM(time_iowait)+SUM(time_steal))::float*100)::numeric, 1) AS user, round((SUM(time_system)/(SUM(time_user)+SUM(time_system)+SUM(time_idle)+SUM(time_iowait)+SUM(time_steal))::float*100)::numeric, 1) AS system, round((SUM(time_iowait)/(SUM(time_user)+SUM(time_system)+SUM(time_idle)+SUM(time_iowait)+SUM(time_steal))::float*100)::numeric, 1) AS iowait, round((SUM(time_steal)/(SUM(time_user)+SUM(time_system)+SUM(time_idle)+SUM(time_iowait)+SUM(time_steal))::float*100)::numeric, 1) AS steal FROM supervision.%s WHERE hostname = '%s' AND datetime >= '%s' AND datetime <= '%s' GROUP BY 1,hostname ORDER BY 1) TO STDOUT WITH CSV HEADER"
+    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, round((SUM(time_user)/(SUM(time_user)+SUM(time_system)+SUM(time_idle)+SUM(time_iowait)+SUM(time_steal))::float*100)::numeric, 1) AS user, round((SUM(time_system)/(SUM(time_user)+SUM(time_system)+SUM(time_idle)+SUM(time_iowait)+SUM(time_steal))::float*100)::numeric, 1) AS system, round((SUM(time_iowait)/(SUM(time_user)+SUM(time_system)+SUM(time_idle)+SUM(time_iowait)+SUM(time_steal))::float*100)::numeric, 1) AS iowait, round((SUM(time_steal)/(SUM(time_user)+SUM(time_system)+SUM(time_idle)+SUM(time_iowait)+SUM(time_steal))::float*100)::numeric, 1) AS steal FROM supervision.%s WHERE hostname = '%s' AND datetime >= '%s' AND datetime <= '%s' GROUP BY datetime, hostname ORDER BY datetime) TO STDOUT WITH CSV HEADER"
 
     cur = session.connection().connection.cursor()
     cur.copy_expert(query % (tablename, hostname, start.strftime('%Y-%m-%dT%H:%M:%S'), end.strftime('%Y-%m-%dT%H:%M:%S')), data_buffer)
@@ -71,7 +71,7 @@ def get_tps(session, hostname, port, start, end):
     else:
         tablename = 'metric_xacts_4h'
 
-    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, round(SUM(n_commit)/(extract('epoch' from MIN(measure_interval)))) AS commit, round(SUM(n_rollback)/(extract('epoch' from MIN(measure_interval)))) AS rollback FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' GROUP BY datetime, hostname, port ORDER BY 1) TO STDOUT WITH CSV HEADER"
+    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, round(SUM(n_commit)/(extract('epoch' from MIN(measure_interval)))) AS commit, round(SUM(n_rollback)/(extract('epoch' from MIN(measure_interval)))) AS rollback FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' GROUP BY datetime, hostname, port ORDER BY datetime) TO STDOUT WITH CSV HEADER"
 
     cur = session.connection().connection.cursor()
     cur.copy_expert(query % (tablename, hostname, port, start.strftime('%Y-%m-%dT%H:%M:%S'), end.strftime('%Y-%m-%dT%H:%M:%S')), data_buffer)
@@ -102,7 +102,7 @@ def get_db_size(session, hostname, port, start, end):
         col += ", COALESCE(%s,0) AS %s" % (row[0], row[0])
         col_type += ", %s BIGINT" % (row[0])
 
-    q_copy = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS Date %s FROM crosstab('SELECT datetime, dbname, size FROM supervision.%s WHERE hostname = ''%s'' AND port = %s AND datetime >= ''%s'' AND datetime <= ''%s'' ORDER BY 1,2 ASC', 'SELECT DISTINCT(dbname) FROM supervision.%s WHERE datetime >= ''%s'' AND datetime <= ''%s'' ORDER BY 1') AS ct(datetime timestamp with time zone %s)) TO STDOUT WITH CSV HEADER"
+    q_copy = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS Date %s FROM crosstab('SELECT datetime, dbname, size FROM supervision.%s WHERE hostname = ''%s'' AND port = %s AND datetime >= ''%s'' AND datetime <= ''%s'' ORDER BY 1,2 ASC', 'SELECT DISTINCT(dbname) FROM supervision.%s WHERE datetime >= ''%s'' AND datetime <= ''%s'' ORDER BY datetime') AS ct(datetime timestamp with time zone %s)) TO STDOUT WITH CSV HEADER"
     cur = session.connection().connection.cursor()
     str_start_date = start.strftime('%Y-%m-%dT%H:%M:%S')
     str_end_date = end.strftime('%Y-%m-%dT%H:%M:%S')
@@ -148,7 +148,7 @@ def get_memory(session, hostname, start, end):
     else:
         tablename = 'metric_memory_4h'
 
-    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, mem_free AS free, mem_cached AS cached, mem_buffers AS buffers, (mem_used - mem_cached - mem_buffers) AS other  FROM supervision.%s WHERE hostname = '%s' AND datetime >= '%s' AND datetime <= '%s' ORDER BY 1) TO STDOUT WITH CSV HEADER"
+    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, mem_free AS free, mem_cached AS cached, mem_buffers AS buffers, (mem_used - mem_cached - mem_buffers) AS other  FROM supervision.%s WHERE hostname = '%s' AND datetime >= '%s' AND datetime <= '%s' ORDER BY datetime) TO STDOUT WITH CSV HEADER"
 
     cur = session.connection().connection.cursor()
     cur.copy_expert(query % (tablename, hostname, start.strftime('%Y-%m-%dT%H:%M:%S'), end.strftime('%Y-%m-%dT%H:%M:%S')), data_buffer)
@@ -169,7 +169,7 @@ def get_swap(session, hostname, start, end):
     else:
         tablename = 'metric_memory_4h'
 
-    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, swap_used AS used FROM supervision.%s WHERE hostname = '%s' AND datetime >= '%s' AND datetime <= '%s' ORDER BY 1) TO STDOUT WITH CSV HEADER"
+    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, swap_used AS used FROM supervision.%s WHERE hostname = '%s' AND datetime >= '%s' AND datetime <= '%s' ORDER BY datetime) TO STDOUT WITH CSV HEADER"
 
     cur = session.connection().connection.cursor()
     cur.copy_expert(query % (tablename, hostname, start.strftime('%Y-%m-%dT%H:%M:%S'), end.strftime('%Y-%m-%dT%H:%M:%S')), data_buffer)
@@ -190,7 +190,7 @@ def get_sessions(session, hostname, port, start, end):
     else:
         tablename = 'metric_sessions_4h'
 
-    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, SUM(active) AS active, SUM(waiting) AS waiting, SUM(idle) AS idle, SUM(idle_in_xact) AS idle_in_xact, SUM(idle_in_xact_aborted) AS idle_in_xact_aborted, SUM(fastpath) AS fastpath, SUM(disabled) AS disabled FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' GROUP BY 1, hostname, port ORDER BY 1) TO STDOUT WITH CSV HEADER"
+    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, SUM(active) AS active, SUM(waiting) AS waiting, SUM(idle) AS idle, SUM(idle_in_xact) AS idle_in_xact, SUM(idle_in_xact_aborted) AS idle_in_xact_aborted, SUM(fastpath) AS fastpath, SUM(disabled) AS disabled FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' GROUP BY datetime, hostname, port ORDER BY datetime) TO STDOUT WITH CSV HEADER"
 
     cur = session.connection().connection.cursor()
     cur.copy_expert(query % (tablename, hostname, port, start.strftime('%Y-%m-%dT%H:%M:%S'), end.strftime('%Y-%m-%dT%H:%M:%S')), data_buffer)
@@ -211,7 +211,7 @@ def get_blocks(session, hostname, port, start, end):
     else:
         tablename = 'metric_blocks_4h'
 
-    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, ROUND(SUM(blks_read)/(extract('epoch' from MIN(measure_interval)))) AS blks_read_s, ROUND(SUM(blks_hit)/(extract('epoch' from MIN(measure_interval)))) AS blks_hit_s FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' GROUP BY 1, hostname, port ORDER BY 1) TO STDOUT WITH CSV HEADER"
+    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, ROUND(SUM(blks_read)/(extract('epoch' from MIN(measure_interval)))) AS blks_read_s, ROUND(SUM(blks_hit)/(extract('epoch' from MIN(measure_interval)))) AS blks_hit_s FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' GROUP BY datetime, hostname, port ORDER BY datetime) TO STDOUT WITH CSV HEADER"
 
     cur = session.connection().connection.cursor()
     cur.copy_expert(query % (tablename, hostname, port, start.strftime('%Y-%m-%dT%H:%M:%S'), end.strftime('%Y-%m-%dT%H:%M:%S')), data_buffer)
@@ -232,7 +232,7 @@ def get_hitreadratio(session, hostname, port, start, end):
     else:
         tablename = 'metric_blocks_4h'
 
-    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, ROUND((SUM(blks_hit)::FLOAT/(SUM(blks_hit) + SUM(blks_read)::FLOAT) * 100)::numeric, 2) AS hit_read_ratio FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' GROUP BY 1, hostname, port ORDER BY 1) TO STDOUT WITH CSV HEADER"
+    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, ROUND((SUM(blks_hit)::FLOAT/(SUM(blks_hit) + SUM(blks_read)::FLOAT) * 100)::numeric, 2) AS hit_read_ratio FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' GROUP BY datetime, hostname, port ORDER BY datetime) TO STDOUT WITH CSV HEADER"
 
     cur = session.connection().connection.cursor()
     cur.copy_expert(query % (tablename, hostname, port, start.strftime('%Y-%m-%dT%H:%M:%S'), end.strftime('%Y-%m-%dT%H:%M:%S')), data_buffer)
@@ -253,7 +253,7 @@ def get_checkpoints(session, hostname, port, start, end):
     else:
         tablename = 'metric_bgwriter_4h'
 
-    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, checkpoints_timed AS timed, checkpoints_req AS req, ROUND((checkpoint_write_time/1000)::numeric, 1) AS write_time, ROUND((checkpoint_sync_time/1000)::numeric,1) AS sync_time FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' ORDER BY 1) TO STDOUT WITH CSV HEADER"
+    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, checkpoints_timed AS timed, checkpoints_req AS req, ROUND((checkpoint_write_time/1000)::numeric, 1) AS write_time, ROUND((checkpoint_sync_time/1000)::numeric,1) AS sync_time FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' ORDER BY datetime) TO STDOUT WITH CSV HEADER"
 
     cur = session.connection().connection.cursor()
     cur.copy_expert(query % (tablename, hostname, port, start.strftime('%Y-%m-%dT%H:%M:%S'), end.strftime('%Y-%m-%dT%H:%M:%S')), data_buffer)
@@ -274,7 +274,7 @@ def get_written_buffers(session, hostname, port, start, end):
     else:
         tablename = 'metric_bgwriter_4h'
 
-    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, buffers_checkpoint AS checkpoint, buffers_clean AS clean, buffers_backend AS backend FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' ORDER BY 1) TO STDOUT WITH CSV HEADER"
+    query = "COPY (SELECT to_char(datetime, 'YYYY/MM/DD HH24:MI:SS') AS date, buffers_checkpoint AS checkpoint, buffers_clean AS clean, buffers_backend AS backend FROM supervision.%s WHERE hostname = '%s' AND port = %s AND datetime >= '%s' AND datetime <= '%s' ORDER BY datetime) TO STDOUT WITH CSV HEADER"
 
     cur = session.connection().connection.cursor()
     cur.copy_expert(query % (tablename, hostname, port, start.strftime('%Y-%m-%dT%H:%M:%S'), end.strftime('%Y-%m-%dT%H:%M:%S')), data_buffer)
