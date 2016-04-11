5ffa5b345dc8a187298c895b021ef919b973ada3
Julien Tachoires <julmon@gmail.com>
2016-04-11 10:00:21 +0200

    [settings] A JS function was missing.

--- ganeshwebui/plugins/settings/static/js/ganesh.settings.js
+++ ganeshwebui/plugins/settings/static/js/ganesh.settings.js
@@ -2,6 +2,37 @@
  * Settings plugin
  */
 
+function modal_api_call(api_host, api_port, api_url, api_method, xsession, modal_id, json_params)
+{
+	$.ajax({
+		url: '/proxy/'+api_host+'/'+api_port+api_url,
+		type: api_method,
+		data: JSON.stringify(json_params),
+		beforeSend: function(xhr){
+			$('#'+modal_id+'Label').html('Processing, please wait...');
+			$('#'+modal_id+'Body').html('<div class="row"><div class="col-md-4 col-md-offset-4"><div class="progress"><div class="progress-bar progress-bar-striped active" style="width: 100%;">Please wait ...</div></div></div></div>');
+			$('#'+modal_id+'Footer').html('<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>');
+			xhr.setRequestHeader('X-Session', xsession);
+		},
+		async: true,
+		contentType: "application/json",
+		dataType: "json",
+		success: function (data) {
+			var url = window.location.href;
+			window.location.replace(url);
+		},
+		error: function(xhr) {
+			if (xhr.status == 401)
+			{
+				$('#'+modal_id+'Body').html('<div class="alert alert-danger" role="alert">Session expired. <a class="btn btn-danger" id="ConfirmOK" href="/server/'+api_host+'/'+api_port+'/login">Back to login page</a></div>');
+			}
+			else
+			{
+				$('#'+modal_id+'Body').html('<div class="alert alert-danger" role="alert">ERROR: '+render_xhr_error(xhr)+'</div>');
+			}
+		}
+	});
+}
 
 /*
  * Move up/down a table row (tr).
