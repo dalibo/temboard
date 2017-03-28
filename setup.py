8eb328e0d9432a4fee005cbbc9eef6fffb922c33
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-03-28 16:09:12 +0200

    Read version from git tag

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 from setuptools import setup, find_packages
+import subprocess
+
+
+try:
+    # Release mode
+    VERSION = (
+        subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
+        .strip().decode()
+    )
+except subprocess.CalledProcessError:
+    # pip install mode
+    with open('PKG-INFO') as fo:
+        for line in fo:
+            if not line.startswith('Version: '):
+                continue
+            VERSION = line.replace('Version: ', '').strip()
+            break
 
 setup(
     name='temboard-agent',
-    version='0.0.1',
+    version=VERSION,
     author='Dalibo',
     license='PostgreSQL',
     packages=find_packages(),
