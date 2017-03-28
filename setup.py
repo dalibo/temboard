fdbfed6b7888b24d4ab39c7e1baf2c54246cdef3
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-03-28 10:37:19 +0200

    YAPF setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,20 @@
-from setuptools import setup
-
-requires = [
-]
+from setuptools import setup, find_packages
 
 setup(
-    name = 'temboard-agent',
-    version = '0.0.1',
-    author = 'Dalibo',
-    license = 'PostgreSQL',
-    packages = [
-                'temboardagent',
-                'temboardagent.plugins.supervision',
-                'temboardagent.plugins.dashboard',
-                'temboardagent.plugins.administration',
-                'temboardagent.plugins.settings',
-                'temboardagent.plugins.activity'],
-    scripts = [
-                'temboard-agent',
-                'temboard-agent-password',
-                'temboard-agent-adduser'],
-    url = '',
-    description = 'Administration & monitoring PostgreSQL agent.',
-    data_files = [('share/temboard-agent/', [
-                                            'share/temboard-agent.conf.sample',
-                                            'share/temboard-agent_CHANGEME.pem',
-                                            'share/temboard-agent_CHANGEME.key',
-                                            'share/temboard-agent_ca_certs_CHANGEME.pem',
-                                            'share/temboard-agent.logrotate'])]
-)
+    name='temboard-agent',
+    version='0.0.1',
+    author='Dalibo',
+    license='PostgreSQL',
+    packages=find_packages(),
+    scripts=[
+        'temboard-agent', 'temboard-agent-password', 'temboard-agent-adduser'
+    ],
+    url='http://temboard.io/',
+    description='Administration & monitoring PostgreSQL agent.',
+    data_files=[('share/temboard-agent/', [
+        'share/temboard-agent.conf.sample',
+        'share/temboard-agent_CHANGEME.pem',
+        'share/temboard-agent_CHANGEME.key',
+        'share/temboard-agent_ca_certs_CHANGEME.pem',
+        'share/temboard-agent.logrotate'
+    ])])
