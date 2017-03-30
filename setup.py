0f2040f1ce2d75cf559c8688606aa0b58c74e52a
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-03-30 11:20:50 +0200

    List scripts one entry per line

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setup(
     license='PostgreSQL',
     packages=find_packages(),
     scripts=[
-        'temboard-agent', 'temboard-agent-password', 'temboard-agent-adduser'
+        'temboard-agent',
+        'temboard-agent-adduser',
+        'temboard-agent-password',
     ],
     url='http://temboard.io/',
     description='Administration & monitoring PostgreSQL agent.',
