c076fefdc0ed6ad9cd18d79c807bb7a73e216e46
Julien Tachoires <julmon@gmail.com>
2017-04-01 15:00:34 +0200

    flake8 on options.py

--- temboardagent/options.py
+++ temboardagent/options.py
@@ -1,13 +1,18 @@
 from optparse import OptionParser
 
+
 class CLIOptions(OptionParser):
     """
     Command line interface options parser.
     """
     def __init__(self, *args, **kwargs):
         OptionParser.__init__(self, *args, **kwargs)
-        self.add_option("-c", "--config", dest="configfile",
-            help="Configuration file. Default: %default", default="/etc/temboard-agent/temboard-agent.conf")
+        self.add_option("-c",
+                        "--config",
+                        dest="configfile",
+                        help="Configuration file. Default: %default",
+                        default="/etc/temboard-agent/temboard-agent.conf")
+
 
 class temboardOptions(CLIOptions):
     """
@@ -15,10 +20,18 @@ class temboardOptions(CLIOptions):
     """
     def __init__(self, *args, **kwargs):
         CLIOptions.__init__(self, *args, **kwargs)
-        self.add_option("-d", "--daemon", dest="daemon", action="store_true",
-            help="Run in background. Default: %default", default=False)
-        self.add_option("-p", "--pid-file", dest="pidfile",
-            help="PID file. Default: %default", default="/run/temboard-agent.pid")
+        self.add_option("-d",
+                        "--daemon",
+                        dest="daemon",
+                        action="store_true",
+                        help="Run in background. Default: %default",
+                        default=False)
+        self.add_option("-p",
+                        "--pid-file",
+                        dest="pidfile",
+                        help="PID file. Default: %default",
+                        default="/run/temboard-agent.pid")
+
 
 class agentRegisterOptions(CLIOptions):
     """
@@ -26,12 +39,24 @@ class agentRegisterOptions(CLIOptions):
     """
     def __init__(self, *args, **kwargs):
         CLIOptions.__init__(self, *args, **kwargs)
-        self.add_option("-h", "--host", dest="host",
-            help="Agent address. Default: %default", default="localhost")
-        self.add_option("-p", "--port", dest="port",
-            help="Agent port. Default: %default", default="2345")
-        self.add_option("-g", "--groups", dest="groups",
-            help="Instance groups list, comma separated. Default: %default", default=None)
-        self.add_option("--help", dest = "help",
-            action = "store_true",
-            help = "Show this help message and exit.", default = False)
+        self.add_option("-h",
+                        "--host",
+                        dest="host",
+                        help="Agent address. Default: %default",
+                        default="localhost")
+        self.add_option("-p",
+                        "--port",
+                        dest="port",
+                        help="Agent listening TCP port. Default: %default",
+                        default="2345")
+        self.add_option("-g",
+                        "--groups",
+                        dest="groups",
+                        help="Instance groups list, comma separated. "
+                             "Default: %default",
+                        default=None)
+        self.add_option("--help",
+                        dest="help",
+                        action="store_true",
+                        help="Show this help message and exit.",
+                        default=False)
