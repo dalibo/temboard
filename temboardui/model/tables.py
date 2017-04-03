f56920c24435c6eb96796a5a3b19e00c6babd017
Pierre GIRAUD <pierre.giraud@dalibo.com>
2017-04-03 09:16:00 +0200

    Flake8 tables.py

--- temboardui/model/tables.py
+++ temboardui/model/tables.py
@@ -1,11 +1,18 @@
 from sqlalchemy.sql import text
 
 from sqlalchemy.schema import (
-    MetaData, Table, Column,
-    ForeignKey, CheckConstraint, ForeignKeyConstraint)
+    MetaData,
+    Table,
+    Column,
+    CheckConstraint,
+    ForeignKeyConstraint
+)
 from sqlalchemy.types import (
-    Integer, UnicodeText, BigInteger,
-    Boolean, DateTime)
+    Integer,
+    UnicodeText,
+    BigInteger,
+    Boolean,
+)
 
 metadata = MetaData()
 
@@ -16,7 +23,7 @@ roles = Table(
     Column('role_email', UnicodeText, nullable=False),
     Column('is_active', Boolean, nullable=False, server_default=text('True')),
     Column('is_admin', Boolean, nullable=False, server_default=text('False')),
-    schema = "application")
+    schema="application")
 
 groups = Table(
     'groups', metadata,
@@ -24,7 +31,7 @@ groups = Table(
     Column('group_description', UnicodeText),
     Column('group_kind', UnicodeText, nullable=False, primary_key=True),
     CheckConstraint("group_kind IN ('instance', 'role')"),
-    schema = "application")
+    schema="application")
 
 instances = Table(
     'instances', metadata,
@@ -37,7 +44,7 @@ instances = Table(
     Column('pg_port', Integer),
     Column('pg_version', UnicodeText),
     Column('pg_data', UnicodeText),
-    schema = "application")
+    schema="application")
 
 plugins = Table(
     'plugins', metadata,
@@ -45,51 +52,62 @@ plugins = Table(
     Column('agent_port', Integer, nullable=False, primary_key=True),
     Column('plugin_name', UnicodeText, nullable=False, primary_key=True),
     ForeignKeyConstraint(['agent_address', 'agent_port'],
-                         ['application.instances.agent_address', 'application.instances.agent_port'],
+                         ['application.instances.agent_address',
+                          'application.instances.agent_port'],
                          ondelete="CASCADE", onupdate="CASCADE"),
-    schema = "application")
+    schema="application")
 
 instance_groups = Table(
     'instance_groups', metadata,
     Column('agent_address', UnicodeText, nullable=False, primary_key=True),
     Column('agent_port', Integer, nullable=False, primary_key=True),
     Column('group_name', UnicodeText, nullable=False, primary_key=True),
-    Column('group_kind', UnicodeText, nullable=False, server_default=text('instance')),
+    Column('group_kind', UnicodeText, nullable=False,
+           server_default=text('instance')),
     CheckConstraint("group_kind = 'instance'"),
     ForeignKeyConstraint(['agent_address', 'agent_port'],
-                         ['application.instances.agent_address', 'application.instances.agent_port'],
+                         ['application.instances.agent_address',
+                          'application.instances.agent_port'],
                          ondelete="CASCADE", onupdate="CASCADE"),
     ForeignKeyConstraint(['group_name', 'group_kind'],
-                         ['application.groups.group_name', 'application.groups.group_kind'],
+                         ['application.groups.group_name',
+                          'application.groups.group_kind'],
                          ondelete="CASCADE", onupdate="CASCADE"),
-    schema = "application")
+    schema="application")
 
 role_groups = Table(
     'role_groups', metadata,
     Column('role_name', UnicodeText, nullable=False, primary_key=True),
     Column('group_name', UnicodeText, nullable=False, primary_key=True),
-    Column('group_kind', UnicodeText, nullable=False, server_default=text('role')),
+    Column('group_kind', UnicodeText, nullable=False,
+           server_default=text('role')),
     CheckConstraint("group_kind = 'role'"),
     ForeignKeyConstraint(['role_name'],
                          ['application.roles.role_name'],
                          ondelete="CASCADE", onupdate="CASCADE"),
     ForeignKeyConstraint(['group_name', 'group_kind'],
-                         ['application.groups.group_name', 'application.groups.group_kind'],
+                         ['application.groups.group_name',
+                          'application.groups.group_kind'],
                          ondelete="CASCADE", onupdate="CASCADE"),
-    schema = "application")
+    schema="application")
 
 access_role_instance = Table(
     'access_role_instance', metadata,
     Column('role_group_name', UnicodeText, nullable=False, primary_key=True),
-    Column('role_group_kind', UnicodeText, nullable=False, server_default = text('role')),
-    Column('instance_group_name', UnicodeText, nullable=False, primary_key=True),
-    Column('instance_group_kind', UnicodeText, nullable=False, server_default = text('instance')),
+    Column('role_group_kind', UnicodeText, nullable=False,
+           server_default=text('role')),
+    Column('instance_group_name', UnicodeText, nullable=False,
+           primary_key=True),
+    Column('instance_group_kind', UnicodeText, nullable=False,
+           server_default=text('instance')),
     CheckConstraint("role_group_kind = 'role'"),
     CheckConstraint("instance_group_kind = 'instance'"),
     ForeignKeyConstraint(['role_group_name', 'role_group_kind'],
-                         ['application.groups.group_name', 'application.groups.group_kind'],
+                         ['application.groups.group_name',
+                          'application.groups.group_kind'],
                          ondelete="CASCADE", onupdate="CASCADE"),
     ForeignKeyConstraint(['instance_group_name', 'instance_group_kind'],
-                         ['application.groups.group_name', 'application.groups.group_kind'],
+                         ['application.groups.group_name',
+                          'application.groups.group_kind'],
                          ondelete="CASCADE", onupdate="CASCADE"),
-    schema = "application")
+    schema="application")
