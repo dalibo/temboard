f4cd943026d0f17ee42aaf3452400fd8d95fb59e
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-04-18 14:54:06 +0200

    Deploy systemd unit with setuptools

--- setup.py
+++ setup.py
@@ -44,13 +44,16 @@ setup(
     include_package_data=True,
     zip_safe=False,
     url='https://github.com/dalibo/temboard/',
-    data_files=[('share/temboard/', [
-        'share/sql/application.sql',
-        'share/sql/dev-fixture.sql',
-        'share/ssl/temboard_CHANGEME.key',
-        'share/ssl/temboard_CHANGEME.pem',
-        'share/ssl/temboard_ca_certs_CHANGEME.pem',
-        'share/temboard.conf.sample',
-        'share/temboard.logrotate',
-        'temboardui/plugins/monitoring/sql/monitoring.sql'
-    ])])
+    data_files=[
+        ('share/temboard/', [
+            'share/sql/application.sql',
+            'share/sql/dev-fixture.sql',
+            'share/ssl/temboard_CHANGEME.key',
+            'share/ssl/temboard_CHANGEME.pem',
+            'share/ssl/temboard_ca_certs_CHANGEME.pem',
+            'share/temboard.conf.sample',
+            'share/temboard.logrotate',
+            'temboardui/plugins/monitoring/sql/monitoring.sql',
+        ]),
+        ('lib/systemd/system', ['temboard.service']),
+    ])
