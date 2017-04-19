cc4439c99586dc1cf071be64effae802ff2d9444
Pierre GIRAUD <pierre.giraud@dalibo.com>
2017-04-19 09:58:11 +0200

    Flake8 errors.py

--- temboardui/errors.py
+++ temboardui/errors.py
@@ -1,14 +1,18 @@
 class CLIError(Exception):
     """ CLI errors """
+
     def __init__(self, message):
         Exception.__init__(self, message)
         self.message = str(message)
 
+
 class ConfigurationError(Exception):
     """ Configuration errors """
+
     def __init__(self, message):
         Exception.__init__(self, message)
         self.message = str(message)
+
 
 class TemboardUIError(Exception):
     def __init__(self, code, message):
