275ac9f61103bd8cf55ddcf3e51d5291deec6d8e
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-09-27 14:48:07 +0200

    Configure logging **once**

--- temboardagent/logger.py
+++ temboardagent/logger.py
@@ -1,80 +1,28 @@
-import socket
-from logging import (Logger, Formatter, DEBUG, INFO, WARNING, ERROR, CRITICAL,
-                     FileHandler, StreamHandler)
+import logging
 from logging.handlers import SysLogHandler
-from temboardagent.errors import ConfigurationError
 
 
-# Mapping configuration -> constants
-LOG_FACILITIES = {
-    'local0': SysLogHandler.LOG_LOCAL0,
-    'local1': SysLogHandler.LOG_LOCAL1,
-    'local2': SysLogHandler.LOG_LOCAL2,
-    'local3': SysLogHandler.LOG_LOCAL3,
-    'local4': SysLogHandler.LOG_LOCAL4,
-    'local5': SysLogHandler.LOG_LOCAL5,
-    'local6': SysLogHandler.LOG_LOCAL6,
-    'local7': SysLogHandler.LOG_LOCAL7
+LOG_METHODS = {
+    'stderr': {
+        '()': 'logging.StreamHandler',
+        'formatter': 'full',
+    },
+    'file': {
+        '()': 'logging.FileHandler',
+        'mode': 'a',
+        'formatter': 'minimal',
+    },
+    'syslog': {
+        '()': 'logging.handlers.SysLogHandler',
+        'formatter': 'minimal',
+    }
 }
 
-LOG_LEVELS = {
-    'DEBUG': DEBUG,
-    'INFO': INFO,
-    'WARNING': WARNING,
-    'ERROR': ERROR,
-    'CRITICAL': CRITICAL
-}
-
-LOG_METHODS = ['syslog', 'file', 'stderr']
+LOG_FACILITIES = SysLogHandler.facility_names
+LOG_LEVELS = logging._levelNames.values()
 LOG_FORMAT = (
     "temboard-agent[%(process)d]: [%(name)s] %(levelname)s: %(message)s"
 )
-
-
-class Log(Logger):
-    """
-    Logger
-    """
-    _instances = dict()
-
-    def __new__(cls, config, name, *args, **kwargs):
-        # Create a new logger only once.
-        if name not in cls._instances:
-            inst = super(Log, cls).__new__(cls)
-            cls._instances[name] = inst
-        return cls._instances[name]
-
-    def __init__(self, config, name, *args, **kwargs):
-        Logger.__init__(self, name, *args, **kwargs)
-
-        log_format = LOG_FORMAT
-        if config.logging['method'] == 'syslog':
-            try:
-                # Instanciate a new syslog handler.
-                lh = SysLogHandler(
-                        address=str(config.logging['destination']),
-                        facility=LOG_FACILITIES[config.logging['facility']])
-            except socket.error as e:
-                raise ConfigurationError(e)
-        elif config.logging['method'] == 'file':
-            try:
-                # Instanciate a new file handler.
-                lh = FileHandler(
-                        filename=config.logging['destination'],
-                        mode='a')
-                # Add timestamp when using a FileHandler.
-                log_format = "%(asctime)s "+log_format
-            except IOError as e:
-                raise ConfigurationError(e)
-        elif config.logging['method'] == 'stderr':
-            lh = StreamHandler()
-            # Add timestamp
-            log_format = "%(asctime)s "+log_format
-
-        # Set log level according to the level defined in configuration files.
-        lh.setLevel(LOG_LEVELS[config.logging['level']])
-        lh.setFormatter(Formatter(log_format))
-        self.addHandler(lh)
 
 
 LOGGER_NAME = 'temboard-agent'
@@ -89,4 +37,40 @@ def get_logger(config):
     """
     Returns a logger instance.
     """
-    return Log(config, LOGGER_NAME)
+    return logging.getLogger(LOGGER_NAME)
+
+
+def generate_logging_config(config):
+    LOG_METHODS['file']['filename'] = config.logging['destination']
+    facility = SysLogHandler.facility_names[config.logging['facility']]
+    LOG_METHODS['syslog']['facility'] = facility
+    LOG_METHODS['syslog']['address'] = config.logging['destination']
+
+    logging_config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'minimal': {
+                'format': LOG_FORMAT,
+            },
+            'full': {
+                'format': '%(asctime)s ' + LOG_FORMAT,
+            }
+        },
+        'handlers': {
+            'configured': LOG_METHODS[config.logging['method']]
+        },
+        'root': {
+            'level': 'INFO',
+            'handlers': ['configured'],
+        },
+        'loggers': {
+            'temboardagent': {
+                'level': config.logging['level'],
+            },
+            'temboard-agent': {
+                'level': config.logging['level'],
+            },
+        },
+    }
+    return logging_config
