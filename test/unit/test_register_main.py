38d56717df32d5935a2e43aed52ec58a160ea104
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-09-29 08:01:16 +0200

    Move scripts to temboardagent.scripts package

--- test/unit/test_register_main.py
+++ test/unit/test_register_main.py
@@ -2,9 +2,9 @@ import pytest
 
 
 def test_big_main(mocker):
-    mocker.patch('temboardagent.register.LazyConfiguration')
+    mocker.patch('temboardagent.scripts.register.LazyConfiguration')
 
-    from temboardagent.register import main
+    from temboardagent.scripts.register import main
 
     with pytest.raises(SystemExit):
         main(argv=['--help'])
