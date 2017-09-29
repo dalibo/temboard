c46c554b668480170da63d49d81a04600ec1af92
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-09-29 09:21:20 +0200

    Don't define default configuration file in arguments

--- temboardagent/cli.py
+++ temboardagent/cli.py
@@ -15,7 +15,6 @@ def define_common_arguments(parser):
     parser.add_argument(
         '-c', '--config',
         action='store', dest='temboard_configfile',
-        default='/etc/temboard-agent/temboard-agent.conf',
         help="Configuration file. Default: %(default)s",
     )
 
