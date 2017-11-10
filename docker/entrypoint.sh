f499d12c8edc1c908be573d5e47347f73a70537f
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-11-10 15:36:18 +0100

    Use certs from quickstart/ direcotry

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,6 +8,10 @@ export PGUSER=${PGUSER-postgres}
 PGPASSWORD=${PGPASSWORD-}
 export PGDATABASE=${PGDATABASE-postgres}
 
+TEMBOARD_UI_URL=${TEMBOARD_UI_URL-}
+export TEMBOARD_UI_USER=${TEMBOARD_UI_USER-admin}
+export TEMBOARD_UI_PASSWORD=${TEMBOARD_UI_PASSWORD-admin}
+
 COMPOSE_PROJECT=$(docker inspect --format "{{ index .Config.Labels \"com.docker.compose.project\"}}" $HOSTNAME)
 links=($(docker inspect --format "{{range .NetworkSettings.Networks.${COMPOSE_PROJECT}_default.Links }}{{.}} {{end}}" $HOSTNAME))
 links=(${links[@]%%:${TEMBOARD_HOSTNAME}})
@@ -26,8 +30,8 @@ home = /var/lib/temboard
 users = /etc/temboard-agent/users
 address = 0.0.0.0
 port = 2345
-ssl_cert_file = ${TEMBOARD_SSL_CERT}
-ssl_key_file = ${TEMBOARD_SSL_KEY}
+ssl_cert_file = ${TEMBOARD_SSL_CERT-/usr/local/share/temboard-agent/quickstart/temboard-agent_CHANGEME.pem}
+ssl_key_file = ${TEMBOARD_SSL_KEY-/usr/local/share/temboard-agent/quickstart/temboard-agent_CHANGEME.key}
 
 [logging]
 method = stderr
@@ -42,7 +46,7 @@ instance = ${PGINSTANCE-main}
 
 [monitoring]
 collector_url = ${TEMBOARD_UI_URL%/}/monitoring/collector
-ssl_ca_cert_file = ${TEMBOARD_SSL_CA}
+ssl_ca_cert_file = ${TEMBOARD_SSL_CA-/usr/local/share/temboard-agent/quickstart/temboard-agent_ca_certs_CHANGEME.pem}
 
 [administration]
 pg_ctl = docker %s ${PGCONTAINER}
@@ -51,7 +55,7 @@ EOF
 
 touch /etc/temboard-agent/users
 chmod 0600 /etc/temboard-agent/users
-for entry in ${TEMBOARD_USERS_LIST} ; do
+for entry in ${TEMBOARD_USERS_LIST-alice:alice bob:bob} ; do
     echo "Adding user ${entry%%:*}."
     sed -i /${entry%:*}/d /etc/temboard-agent/users
     temboard-agent-password $entry >> /etc/temboard-agent/users
@@ -69,11 +73,11 @@ register() {
     temboard-agent-register \
         --host ${TEMBOARD_REGISTER_HOST-$COMPOSE_SERVICE} \
         --port ${TEMBOARD_REGISTER_PORT-2345} \
-        --groups ${TEMBOARD_GROUPS} \
+        --groups ${TEMBOARD_GROUPS-local_instances} \
         ${TEMBOARD_UI_URL%/}
 }
 
-if [ -z "${command##temboard-agent*}" -a -n "${TEMBOARD_UI_USER-}" ] ; then
+if [ -z "${command##temboard-agent*}" -a -n "${TEMBOARD_UI_USER}" ] ; then
     register &
 fi
 
