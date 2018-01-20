2776c5f3e99f0b103f2ab21c3a3326fd2fed8b14
Julien Tachoires <julmon@gmail.com>
2018-01-20 23:49:44 +0100

    Remove dead code

--- temboardagent/daemon.py
+++ temboardagent/daemon.py
@@ -7,19 +7,7 @@ import time
 # Some global vars we need to have because signal handler function can't take
 # extra parameters.
 PIDFILE = None
-SCHEDULER = None
-WORKERS = None
 RELOAD = False
-
-
-def set_global_workers(workers):
-    global WORKERS
-    WORKERS = workers
-
-
-def set_global_scheduler(scheduler):
-    global SCHEDULER
-    SCHEDULER = scheduler
 
 
 def set_global_reload(value):
@@ -36,81 +24,15 @@ def httpd_sighup_handler(signum, frame):
     SIGHUP handler for httpd process.
     """
     set_global_reload(True)
-    if SCHEDULER:
-        # Send a SIGHUP signal to the scheduler.
-        os.kill(SCHEDULER.pid, signal.SIGHUP)
-
-
-def scheduler_sighup_handler(signum, frame):
-    """
-    SIGHUP handler for scheduler process.
-    """
-    set_global_reload(True)
-    if WORKERS:
-        for worker in WORKERS:
-            if worker.is_alive():
-                worker_pid = worker.pid
-                if worker_pid:
-                    os.kill(worker_pid, signal.SIGHUP)
-            time.sleep(0.5)
 
 
 def httpd_sigterm_handler(signum, frame):
     """
     SIGTERM signal handler for httpd process.
     """
-    if SCHEDULER:
-        # Send a SIGTERM signal to the scheduler.
-        os.kill(SCHEDULER.pid, signal.SIGTERM)
-        # Wait until it dies, because while it's alive, some workers may be
-        # running and we want to wait until all workers finish their work
-        # before exiting.
-        while True:
-            if not SCHEDULER.is_alive():
-                SCHEDULER.join()
-                break
-            time.sleep(0.5)
-    # Remove the pidfile.
-    remove_pidfile(PIDFILE)
     # Exit roughly with os._exit() because httpd is multi-threaded and
     # sys.exit() does not work in this context.
     os._exit(1)
-
-
-def scheduler_sigterm_handler(signum, frame):
-    """
-    SIGTEMR signal handler for scheduler process.
-    """
-    if WORKERS:
-        n_dead_worker = 0
-        while n_dead_worker < len(WORKERS):
-            # Wait until all worker processes are dead.
-            n_dead_worker = 0
-            for worker in WORKERS:
-                if worker.is_alive():
-                    worker_pid = worker.pid
-                    if worker_pid:
-                        os.kill(worker_pid, signal.SIGTERM)
-                if not worker.is_alive():
-                    worker.join()
-                    n_dead_worker += 1
-                    WORKERS.remove(worker)
-            time.sleep(0.5)
-    sys.exit(1)
-
-
-def worker_sigterm_handler(signum, frame):
-    """
-    Default SIGTERM signal handler for the workers.
-    """
-    return
-
-
-def worker_sighup_handler(signum, frame):
-    """
-    Default SIGHUP signal handler for the workers.
-    """
-    return
 
 
 def remove_pidfile(pidfile):
