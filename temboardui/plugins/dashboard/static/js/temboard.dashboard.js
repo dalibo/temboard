587232483645d780c619d445ca6f504da917a96a
Pierre GIRAUD <pierre.giraud@dalibo.com>
2018-03-06 11:58:41 +0100

    ShowAllTooltips plugin not required anymore

--- temboardui/plugins/dashboard/static/js/temboard.dashboard.js
+++ temboardui/plugins/dashboard/static/js/temboard.dashboard.js
@@ -1,52 +1,5 @@
 var last_update_timestamp = 0;
 
-// Chart.js 2.X plugin to force display of tooltips with doughnut charts.
-Chart.pluginService.register({
-  beforeRender: function (chart) {
-    if (chart.config.options.showAllTooltips) {
-      // create an array of tooltips
-      // we can't use the chart tooltip because there is only one tooltip per chart
-      chart.pluginTooltips = [];
-      chart.config.data.datasets.forEach(function (dataset, i) {
-        chart.getDatasetMeta(i).data.forEach(function (sector, j) {
-          chart.pluginTooltips.push(new Chart.Tooltip({
-            _chart: chart.chart,
-            _chartInstance: chart,
-            _data: chart.data,
-            _options: chart.options.tooltips,
-            _active: [sector]
-          }, chart));
-        });
-      });
-
-      // turn off normal tooltips
-      chart.options.tooltips.enabled = false;
-    }
-  },
-  afterDraw: function (chart, easing) {
-    if (chart.config.options.showAllTooltips) {
-      // we don't want the permanent tooltips to animate, so don't do anything till the animation runs atleast once
-      if (!chart.allTooltipsOnce) {
-        if (easing !== 1) {
-          return;
-        }
-        chart.allTooltipsOnce = true;
-      }
-
-      // turn on tooltips
-      chart.options.tooltips.enabled = true;
-      Chart.helpers.each(chart.pluginTooltips, function (tooltip) {
-        tooltip.initialize();
-        tooltip.update();
-        // we don't actually need this since we are not animating tooltips
-        tooltip.pivot();
-        tooltip.transition(easing).draw();
-      });
-      chart.options.tooltips.enabled = false;
-    }
-  }
-});
-
 /*
  * CPU & Memory usage donut charts options.
  */
