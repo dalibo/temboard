31a58b4854fe72eda4ab420355f189fb8ddca851
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-03-16 12:44:41 +0100

    plugins: Port /hello to new plugin

--- test/temboard-agent-sample-plugins/temboard_agent_sample_plugins.py
+++ test/temboard-agent-sample-plugins/temboard_agent_sample_plugins.py
@@ -1,9 +1,39 @@
 from __future__ import unicode_literals
 
 import logging
+import sys
+
+from temboardagent.routing import add_route
+from temboardagent.api_wrapper import api_function_wrapper
 
 
 logger = logging.getLogger('temboardagent.' + __name__)
+
+
+def say_hello_world(config, http_context):
+    """
+    Basic "Hello world" API using HTTP method GET.
+
+    Usage:
+    $ export XSESSION=`curl -s -k -X POST --data '{"username":"<user>", "password":"<password>"}' https://localhost:2345/login | sed -E "s/^.+\"([a-f0-9]+)\".+$/\1/"`
+    $ curl -s -k -H "X-Session:$XSESSION" "https://localhost:2345/hello" | python -m json.tool
+    {
+        "content": "Hello World."
+    }
+    """  # noqa
+    return {"content": "Hello World."}
+
+
+def get_hello(http_context, config=None, sessions=None):
+    """
+    Parameters:
+        http_context: HTTP context containing HTTP paramaters and variables.
+        config: Agent configuration.
+        sessions: List of current sessions.
+    """
+    return api_function_wrapper(
+        config, http_context, sessions,
+        sys.modules[__name__], 'say_hello_world')
 
 
 class Hello(object):
@@ -11,7 +41,7 @@ class Hello(object):
         self.app = app
 
     def load(self):
-        logger.info("hellong plugin loaded.")
+        add_route('GET', b'/hello')(get_hello)
 
 
 class Failing(object):
