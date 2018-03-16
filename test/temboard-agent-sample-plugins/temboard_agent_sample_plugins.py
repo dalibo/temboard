65bae195ce2912a97edfdde2e0afb5cc3e1135f0
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-03-16 15:39:05 +0100

    plugins: Move /hello4 to hellong without shell injection

--- test/temboard-agent-sample-plugins/temboard_agent_sample_plugins.py
+++ test/temboard-agent-sample-plugins/temboard_agent_sample_plugins.py
@@ -8,6 +8,7 @@ from temboardagent.api_wrapper import (
     api_function_wrapper,
     api_function_wrapper_pg,
 )
+from temboardagent.command import exec_command
 from temboardagent.tools import validate_parameters
 from temboardagent.errors import HTTPError
 
@@ -144,6 +145,29 @@ def get_hello_something3(http_context, config=None, sessions=None):
         sys.modules[__name__], 'say_hello_something3')
 
 
+def say_hello_something4(config, http_context):
+    """
+    "Hello <something>" using slug & exec_command.
+
+    Usage:
+    $ export XSESSION=`curl -s -k -X POST --data '{"username":"<user>", "password":"<password>"}' https://localhost:2345/login | sed -E "s/^.+\"([a-f0-9]+)\".+$/\1/"`
+    $ curl -s -k -H "X-Session:$XSESSION" "https://localhost:2345/hello4/toto" | python -m json.tool
+    {
+        "content": "Hello toto"
+    }
+    """  # noqa
+    (return_code, stdout, stderr) = exec_command([
+        'echo', 'Hello', http_context['urlvars'][0],
+    ])
+    return {"content": stdout[:-1]}
+
+
+def get_hello_something4(http_context, config=None, sessions=None):
+    return api_function_wrapper(
+        config, http_context, sessions,
+        sys.modules[__name__], 'say_hello_something4')
+
+
 class Hello(object):
     def __init__(self, app, **kw):
         self.app = app
@@ -155,6 +179,7 @@ class Hello(object):
         add_route('GET', b'/hello/'+T_SOMETHING)(get_hello_something)
         add_route('GET', b'/hello2/say')(get_hello_something2)
         add_route('POST', b'/hello3/say')(get_hello_something3)
+        add_route('GET', b'/hello4/'+T_SOMETHING)(get_hello_something4)
 
 
 class Failing(object):
