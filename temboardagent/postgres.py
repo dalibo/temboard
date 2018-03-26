666fc25843f56310bb3351083b933d24d6ee73c4
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-03-26 11:52:31 +0200

    Add Postgres object to manage Postgres instance

---
+++ temboardagent/postgres.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+from .spc import connector
+
+
+class ConnectionManager(object):
+    def __init__(self, postgres):
+        self.postgres = postgres
+
+    def __enter__(self):
+        self.conn = connector(
+            host=self.postgres.host,
+            port=self.postgres.port,
+            user=self.postgres.user,
+            password=self.postgres.password,
+            database=self.postgres.dbname
+        )
+        self.conn.connect()
+        return self.conn
+
+    def __exit__(self, *a):
+        self.conn.close()
+
+
+class Postgres(object):
+    def __init__(
+            self, host=None, port=5432, user=None, password=None, dbname=None,
+            **kw):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.dbname = dbname
+        self._server_version = None
+
+    def __repr__(self):
+        return '<%s on %s@%s:%s/%s>' % (
+            self.__class__.__name__,
+            self.user, self.host, self.port, self.dbname,
+        )
+
+    def connect(self):
+        return ConnectionManager(self)
+
+    def fetch_version(self):
+        if self._server_version is None:
+            with self.connect() as conn:
+                self._server_version = conn.get_pg_version()
+        return self._server_version
---
+++ test/unit/test_postgres.py
@@ -0,0 +1,26 @@
+def test_postgres_connect(mocker):
+    mocker.patch('temboardagent.postgres.connector', autospec=True)
+
+    from temboardagent.postgres import Postgres
+
+    postgres = Postgres(host='myhost')
+    with postgres.connect() as conn:
+        assert conn
+    assert conn.close.called is True
+
+    assert 'myhost' in repr(postgres)
+
+
+def test_postgres_fetch_version(mocker):
+    c = mocker.patch('temboardagent.postgres.connector', autospec=True)
+
+    conn = c.return_value
+    conn.get_pg_version.return_value = 90400
+
+    from temboardagent.postgres import Postgres
+
+    postgres = Postgres(host='myhost')
+    version = postgres.fetch_version()
+
+    assert 90400 == version
+    assert conn.close.called is True
