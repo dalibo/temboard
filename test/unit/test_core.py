c776d86ffe09e52e6dddb81664039260b40308b2
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-05-11 16:06:01 +0200

    Distinguish master version from released version

---
+++ test/unit/test_core.py
@@ -0,0 +1,7 @@
+def test_version():
+    from pkg_resources import parse_version
+    from temboardagent.version import __version__
+
+    version = parse_version(__version__)
+
+    assert 'Version' == version.__class__.__name__
