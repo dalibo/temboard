d56dad5d02ceee4baceeabce2347ccd85beb6aad
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-11-06 18:09:59 +0100

    rpm: Package from released tarball

--- packaging/rpm/build.sh
+++ packaging/rpm/build.sh
@@ -23,11 +23,25 @@ trap teardown EXIT INT TERM
 sudo yum-builddep -y packaging/rpm/temboard-agent.spec
 sudo sed -i s/.centos// /etc/rpm/macros.dist
 
+# Find source tarball
+if [ -z "${VERSION-}" ] ; then
+	# Find latest published version.
+	VERSION=$(curl https://pypi.debian.net/temboard-agent/ | grep -Po '>temboard-agent-\K.*(?=\.tar\.gz)' | tail -1)
+fi
+tarball=temboard-agent-${VERSION}.tar.gz
+if ! [ -f dist/${tarball} ] ; then
+	# Fetch missing tarball.
+	mkdir -p dist
+	(cd dist/; curl -LO https://pypi.debian.net/temboard-agent/${tarball} )
+	test -f dist/${tarball}
+fi
+ln -f dist/${tarball} packaging/rpm/
+
 # rpmbuild requires files to be owned by running uid
 sudo chown --recursive $(id -u):$(id -g) packaging/rpm/
 
 rpmbuild \
-    --define "pkgversion $(python setup.py --version)" \
+    --define "pkgversion ${VERSION}" \
     --define "_topdir ${PWD}/dist/rpm" \
     --define "_sourcedir ${PWD}/packaging/rpm" \
     -bb packaging/rpm/temboard-agent.spec
@@ -37,5 +51,5 @@ if [ "${DIST}" = "el6" ] ; then
     sudo yum install -y epel-release
 fi
 
-sudo yum install -y dist/rpm/noarch/temboard-agent-*${DIST}*.noarch.rpm
+sudo yum install -y dist/rpm/noarch/temboard-agent-${VERSION}-*${DIST}*.noarch.rpm
 temboard-agent --help
