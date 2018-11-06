8c032e25b232aa5d92c3b89f00ffc1de4668fc5c
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-11-06 16:17:19 +0100

    Noop if systemd is not running

--- share/restart-all.sh
+++ share/restart-all.sh
@@ -5,6 +5,11 @@ if ! hash systemctl &>/dev/null; then
 	exit 0
 fi
 
+if ! systemctl is-system-running &>/dev/null ; then
+	echo You must restart manually temboard-agent services. >&2
+	exit 0
+fi
+
 systemctl daemon-reload
 
 prefix=temboard-agent@
