757d863f1f0f1d57969465c05c1b422d6d0d0765
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-12-07 15:18:13 +0100

    web: Use new tornado application

--- temboardui/__main__.py
+++ temboardui/__main__.py
@@ -5,13 +5,13 @@ import os
 import socket
 import sys
 from argparse import ArgumentParser, SUPPRESS as UNDEFINED_ARGUMENT
+from concurrent.futures import ThreadPoolExecutor
 
 import tornado.ioloop
 import tornado.web
 from tornado import autoreload
 
-from sqlalchemy import create_engine
-
+from .model import configure as configure_db_session
 from .toolkit import taskmanager
 from .toolkit import validators as v
 from .toolkit.app import define_core_arguments
@@ -21,6 +21,7 @@ from .toolkit.services import (
     ServicesManager,
 )
 from .toolkit.log import generate_logging_config
+from .web import app
 from .handlers.base import (
     BaseHandler,
     Error404Handler,
@@ -61,52 +62,10 @@ from .daemon import daemonize
 from .pluginsmgmt import load_plugins
 from .autossl import AutoHTTPSServer
 from .toolkit.app import BaseApplication
-from .utils import check_sqlalchemy_connectivity
 from .version import __version__
 
 
 logger = logging.getLogger('temboardui')
-
-
-class CustomTornadoWebApp(tornado.web.Application):
-    logger = logger
-    config = None
-    loaded_plugins = []
-
-    def load_plugins(self, plugin_names):
-        plugins = load_plugins(plugin_names, self.config)
-        plugins_conf = dict()
-        self.loaded_plugins = []
-        for key, val in plugins.iteritems():
-            self.add_handlers(r'.*', val['routes'])
-            plugins_conf[key] = val['configuration']
-            if key not in self.loaded_plugins:
-                self.loaded_plugins.append(key)
-        return plugins_conf
-
-    def log_request(self, handler):
-        request_time = 1000.0 * handler.request.request_time()
-        log_message = '%d %s %.2fms' % (handler.get_status(),
-                                        handler._request_summary(),
-                                        request_time)
-        logger.info(log_message)
-
-    def create_db_engine(self):
-        dburi = "postgresql://{user}:{pwd}@:{p}/{db}?host={h}".format(
-                    user=self.config.repository['user'],
-                    pwd=self.config.repository['password'],
-                    h=self.config.repository['host'],
-                    p=self.config.repository['port'],
-                    db=self.config.repository['dbname'],
-                )
-        self.engine = create_engine(dburi)
-        try:
-            check_sqlalchemy_connectivity(self.engine)
-        except Exception as e:
-            logger.warning("Connection to the database failed: %s", e)
-            logger.warning("Please check your configuration.")
-            sys.stderr.write("FATAL: %s\n" % e.message)
-            exit(1)
 
 
 def legacy_bootstrap(config):
@@ -127,79 +86,98 @@ def legacy_bootstrap(config):
     return config
 
 
-def make_tornado_app(config):
+def legacy_enable_plugins(self, plugin_names):
+    # Load and enable legacy plugins in tornado app.
+    plugins = load_plugins(plugin_names, self.config)
+    plugins_conf = dict()
+    self.loaded_plugins = []
+    for key, val in plugins.iteritems():
+        self.wildcard_router.add_rules(val['routes'])
+        plugins_conf[key] = val['configuration']
+        if key not in self.loaded_plugins:
+            self.loaded_plugins.append(key)
+    return plugins_conf
+
+
+def setup_tornado_app(app, config):
+    app.config = config
+
     base_path = os.path.dirname(__file__)
     handler_conf = {
         'ssl_ca_cert_file': config.temboard['ssl_ca_cert_file'],
         'template_path': None
     }
-    application = CustomTornadoWebApp(
-        [
-            (r"/", BaseHandler, handler_conf),
-            (r"/home", HomeHandler, handler_conf),
-            (r"/login", LoginHandler, handler_conf),
-            (r"/json/login", LoginJsonHandler, handler_conf),
-            (r"/logout", LogoutHandler, handler_conf),
-            # Manage users
-            (r"/settings/users", SettingsUserHandler, handler_conf),
-            (r"/json/settings/user$", SettingsUserJsonHandler, handler_conf),
-            (r"/json/settings/user/([0-9a-z\-_\.]{3,16})$",
-             SettingsUserJsonHandler, handler_conf),
-            (r"/json/settings/delete/user$", SettingsDeleteUserJsonHandler,
-             handler_conf),
-            (r"/json/settings/all/group/(role|instance)$",
-             SettingsGroupAllJsonHandler, handler_conf),
-            # Manage groups (users & instances)
-            (r"/settings/groups/(role|instance)$", SettingsGroupHandler,
-             handler_conf),
-            (r"/json/settings/group/(role|instance)$",
-             SettingsGroupJsonHandler, handler_conf),
-            (r"/json/settings/group/(role|instance)/([0-9a-z\-_\.]{3,16})$",
-             SettingsGroupJsonHandler, handler_conf),
-            (r"/json/settings/delete/group/(role|instance)$",
-             SettingsDeleteGroupJsonHandler, handler_conf),
-            # Manage instances
-            (r"/settings/instances", SettingsInstanceHandler, handler_conf),
-            (r"/json/settings/instance$", SettingsInstanceJsonHandler,
-             handler_conf),
-            (r"/json/register/instance$", RegisterInstanceJsonHandler,
-             handler_conf),
-            (r"/json/settings/instance/([0-9a-zA-Z\-\._:]+)/([0-9]{1,5})$",
-             SettingsInstanceJsonHandler, handler_conf),
-            (r"/json/settings/delete/instance$",
-             SettingsDeleteInstanceJsonHandler, handler_conf),
-            # Discover
-            (r"/json/discover/instance/([0-9a-zA-Z\-\._:]+)/([0-9]{1,5})$",
-             DiscoverInstanceJsonHandler, handler_conf),
-            # Agent Login
-            (r"/server/(.*)/([0-9]{1,5})/login", AgentLoginHandler,
-             handler_conf),
-            # Notifications
-            (r"/server/(.*)/([0-9]{1,5})/notifications", NotificationsHandler,
-             handler_conf),
-            (r"/css/(.*)", tornado.web.StaticFileHandler, {
-                'path': base_path + '/static/css'
-            }),
-            (r"/js/(.*)", tornado.web.StaticFileHandler, {
-                'path': base_path + '/static/js'
-            }),
-            (r"/images/(.*)", tornado.web.StaticFileHandler, {
-                'path': base_path + '/static/images'
-            }),
-            (r"/fonts/(.*)", tornado.web.StaticFileHandler, {
-                'path': base_path + '/static/fonts'
-            })
-        ],
-        debug=config.logging.debug,
+
+    handlers = [
+        (r"/", BaseHandler, handler_conf),
+        (r"/home", HomeHandler, handler_conf),
+        (r"/login", LoginHandler, handler_conf),
+        (r"/json/login", LoginJsonHandler, handler_conf),
+        (r"/logout", LogoutHandler, handler_conf),
+        # Manage users
+        (r"/settings/users", SettingsUserHandler, handler_conf),
+        (r"/json/settings/user$", SettingsUserJsonHandler, handler_conf),
+        (r"/json/settings/user/([0-9a-z\-_\.]{3,16})$",
+         SettingsUserJsonHandler, handler_conf),
+        (r"/json/settings/delete/user$", SettingsDeleteUserJsonHandler,
+         handler_conf),
+        (r"/json/settings/all/group/(role|instance)$",
+         SettingsGroupAllJsonHandler, handler_conf),
+        # Manage groups (users & instances)
+        (r"/settings/groups/(role|instance)$", SettingsGroupHandler,
+         handler_conf),
+        (r"/json/settings/group/(role|instance)$",
+         SettingsGroupJsonHandler, handler_conf),
+        (r"/json/settings/group/(role|instance)/([0-9a-z\-_\.]{3,16})$",
+         SettingsGroupJsonHandler, handler_conf),
+        (r"/json/settings/delete/group/(role|instance)$",
+         SettingsDeleteGroupJsonHandler, handler_conf),
+        # Manage instances
+        (r"/settings/instances", SettingsInstanceHandler, handler_conf),
+        (r"/json/settings/instance$", SettingsInstanceJsonHandler,
+         handler_conf),
+        (r"/json/register/instance$", RegisterInstanceJsonHandler,
+         handler_conf),
+        (r"/json/settings/instance/([0-9a-zA-Z\-\._:]+)/([0-9]{1,5})$",
+         SettingsInstanceJsonHandler, handler_conf),
+        (r"/json/settings/delete/instance$",
+         SettingsDeleteInstanceJsonHandler, handler_conf),
+        # Discover
+        (r"/json/discover/instance/([0-9a-zA-Z\-\._:]+)/([0-9]{1,5})$",
+         DiscoverInstanceJsonHandler, handler_conf),
+        # Agent Login
+        (r"/server/(.*)/([0-9]{1,5})/login", AgentLoginHandler,
+         handler_conf),
+        # Notifications
+        (r"/server/(.*)/([0-9]{1,5})/notifications", NotificationsHandler,
+         handler_conf),
+        (r"/css/(.*)", tornado.web.StaticFileHandler, {
+            'path': base_path + '/static/css'
+        }),
+        (r"/js/(.*)", tornado.web.StaticFileHandler, {
+            'path': base_path + '/static/js'
+        }),
+        (r"/images/(.*)", tornado.web.StaticFileHandler, {
+            'path': base_path + '/static/images'
+        }),
+        (r"/fonts/(.*)", tornado.web.StaticFileHandler, {
+            'path': base_path + '/static/fonts'
+        })
+    ]
+
+    app.configure(
         cookie_secret=config.temboard['cookie_secret'],
+        debug=config.logging.debug,
         template_path=base_path + "/templates",
-        default_handler_class=Error404Handler)
+        default_handler_class=Error404Handler,
+    )
 
-    application.config = config
-    config.plugins = application.load_plugins(config.temboard['plugins'])
-    application.create_db_engine()
+    config.plugins = legacy_enable_plugins(app, config.temboard['plugins'])
+    # Append rules *after* plugins because plugins shares same namespace for
+    # static rules, i.e. /js/.* is a fallback for /js/dashboard/.*.
+    app.wildcard_router.add_rules(handlers)
 
-    return application
+    return app
 
 
 class SchedulerService(taskmanager.SchedulerService):
@@ -310,6 +288,20 @@ class TemboardApplication(BaseApplication):
     REPORT_URL = "https://github.com/dalibo/temboard/issues"
     VERSION = __version__
 
+    def apply_config(self):
+        if not hasattr(self, 'webapp'):
+            # For now, just create web app once. One time, we'll be able to
+            # unload plugin routes.
+            self.webapp = setup_tornado_app(app, self.config)
+            self.webapp.executor = ThreadPoolExecutor(12)
+            self.webapp.temboard_app = self
+
+        engine = configure_db_session(self.config.repository)
+        # Reuse engine in legacy session maker.
+        self.webapp.engine = engine
+
+        super(TemboardApplication, self).apply_config()
+
     def main(self, argv, environ):
 
         # C O N F I G U R A T I O N
@@ -348,10 +340,7 @@ class TemboardApplication(BaseApplication):
 
         # H T T P   S E R V E R
 
-        self.webapp = make_tornado_app(self.config)
-        self.webapp.temboard_app = self
         webservice = TornadoService(app=self, name=u'main', services=services)
-
         with services:
             webservice.run()
 
