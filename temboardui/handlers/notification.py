e71e36734ef22af56aec76691d7a15b1b00a9eb5
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-12-13 12:26:17 +0100

    Remove web.asynchronous from /notifications

--- temboardui/handlers/notification.py
+++ temboardui/handlers/notification.py
@@ -1,56 +1,19 @@
-import tornado.web
-
-from temboardui.errors import TemboardUIError
-from temboardui.temboardclient import (
-    temboard_get_notifications,
-    temboard_profile,
-)
-from temboardui.handlers.base import BaseHandler
-from temboardui.async import (
-    HTMLAsyncResult,
-    run_background,
+from ..web import (
+    app,
+    render_template,
 )
 
 
-class NotificationsHandler(BaseHandler):
-
-    @BaseHandler.catch_errors
-    def get_notifications(self, agent_address, agent_port):
-        self.logger.info("Getting notifications.")
-
-        self.setUp(agent_address, agent_port)
-
-        xsession = self.get_secure_cookie(
-            "temboard_%s_%s" % (agent_address, agent_port))
-        if not xsession:
-            raise TemboardUIError(401, "Authentication cookie is missing.")
-        else:
-            data_profile = temboard_profile(self.ssl_ca_cert_file,
-                                            agent_address,
-                                            agent_port,
-                                            xsession)
-            agent_username = data_profile['username']
-
-        # Load notifications.
-        notifications = temboard_get_notifications(self.ssl_ca_cert_file,
-                                                   agent_address,
-                                                   agent_port,
-                                                   xsession)
-        self.logger.info("Done.")
-        return HTMLAsyncResult(
-                http_code=200,
-                template_file='notifications.html',
-                data={
-                    'nav': True,
-                    'role': self.current_user,
-                    'instance': self.instance,
-                    'plugin': 'notifications',
-                    'notifications': notifications,
-                    'xsession': xsession,
-                    'agent_username': agent_username,
-                })
-
-    @tornado.web.asynchronous
-    def get(self, agent_address, agent_port):
-        run_background(self.get_notifications, self.async_callback,
-                       (agent_address, agent_port))
+@app.instance_route(r"/notifications")
+def notifications(request):
+    agent_profile = request.instance.get_profile()
+    notifications = request.instance.get_notifications()
+    return render_template(
+        'notifications.html',
+        agent_username=agent_profile['username'],
+        instance=request.instance,
+        nav=True,
+        notifications=notifications,
+        plugin='notifications',
+        role=request.current_user,
+    )
