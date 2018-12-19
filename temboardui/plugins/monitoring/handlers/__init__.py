87f05a5d625c524e682bd686259a2cf3f4a5127e
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-12-19 12:04:48 +0100

    monitoring: Add render_template

--- temboardui/plugins/monitoring/handlers/__init__.py
+++ temboardui/plugins/monitoring/handlers/__init__.py
@@ -1,3 +1,9 @@
-from temboardui.web import Blueprint
+from os.path import realpath
+
+from temboardui.web import (
+    Blueprint,
+    TemplateRenderer,
+)
 
 blueprint = Blueprint()
+render_template = TemplateRenderer(realpath(__file__ + '/../../templates'))
