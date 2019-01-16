fca3c3dd635768da109212358045f2cf4ca18f2d
Julien Tachoires <julmon@gmail.com>
2019-01-16 12:52:48 +0100

    Add CSV data source for the new probes

--- temboardui/plugins/monitoring/chartdata.py
+++ temboardui/plugins/monitoring/chartdata.py
@@ -719,6 +719,129 @@ ORDER BY 1,2 ASC
         """,  # noqa
         probename='bgwriter',
     ),
+    replication_lag=dict(
+        sql_nozoom="""
+SELECT
+    datetime AS date,
+    (record).lag AS lag
+FROM expand_data_by_instance_id('metric_replication_lag', tstzrange(%(start)s, %(end)s), %(instance_id)s)
+AS (datetime timestamp with time zone, instance_id integer, record metric_replication_lag_record)
+ORDER BY 1
+        """,  # noqa
+        sql_zoom="""
+SELECT
+    datetime AS date,
+    (record).lag AS lag
+FROM %(tablename)s
+WHERE instance_id = %(instance_id)s AND datetime >= %(start)s AND datetime <= %(end)s
+ORDER BY 1
+        """,  # noqa
+        probename='replication_lag',
+    ),
+    replication_connection=dict(
+        sql_nozoom="""
+SELECT
+    datetime AS date,
+    upstream AS upstream,
+    (record).connected AS connected
+FROM expand_data_by_instance_id('metric_replication_connection', tstzrange(%(start)s, %(end)s), %(instance_id)s)
+AS (datetime timestamp with time zone, instance_id integer, upstream text, record metric_replication_connection_record)
+ORDER BY 1, 2
+        """,  # noqa
+        sql_zoom="""
+SELECT
+    datetime AS date,
+    upstream AS upstream,
+    (record).connected AS connected
+FROM %(tablename)s
+WHERE instance_id = %(instance_id)s AND datetime >= %(start)s AND datetime <= %(end)s
+ORDER BY 1, 2
+        """,  # noqa
+        probename='replication_connection',
+        pivot=dict(
+            index='date',
+            key='upstream',
+            value='connected',
+        ),
+    ),
+    temp_files_size_delta=dict(
+        sql_nozoom="""
+SELECT
+    datetime AS date,
+    dbname,
+    (record).size
+FROM expand_data_by_instance_id('metric_temp_files_size_delta', tstzrange(%(start)s, %(end)s), %(instance_id)s)
+AS (datetime timestamp with time zone, instance_id integer, dbname text, record metric_temp_files_size_delta_record)
+ORDER BY 1, 2
+        """,  # noqa
+        sql_zoom="""
+SELECT
+    datetime AS date,
+    dbname,
+    (record).size
+FROM %(tablename)s
+WHERE instance_id = %(instance_id)s AND datetime >= %(start)s AND datetime <= %(end)s
+ORDER BY 1, 2
+        """,  # noqa
+        probename='temp_files_size_delta',
+        pivot=dict(
+            index='date',
+            key='dbname',
+            value='size',
+        )
+    ),
+    heap_bloat=dict(
+        sql_nozoom="""
+SELECT
+    datetime AS date,
+    dbname,
+    (record).ratio
+FROM expand_data_by_instance_id('metric_heap_bloat', tstzrange(%(start)s, %(end)s), %(instance_id)s)
+AS (datetime timestamp with time zone, instance_id integer, dbname text, record metric_bloat_ratio_record)
+ORDER BY 1, 2
+        """,  # noqa
+        sql_zoom="""
+SELECT
+    datetime AS date,
+    dbname,
+    (record).ratio
+FROM %(tablename)s
+WHERE instance_id = %(instance_id)s AND datetime >= %(start)s AND datetime <= %(end)s
+ORDER BY 1, 2
+        """,  # noqa
+        probename='heap_bloat',
+        pivot=dict(
+            index='date',
+            key='dbname',
+            value='ratio',
+        )
+    ),
+    btree_bloat=dict(
+        sql_nozoom="""
+SELECT
+    datetime AS date,
+    dbname,
+    (record).ratio
+FROM expand_data_by_instance_id('metric_btree_bloat', tstzrange(%(start)s, %(end)s), %(instance_id)s)
+AS (datetime timestamp with time zone, instance_id integer, dbname text, record metric_bloat_ratio_record)
+ORDER BY 1, 2
+        """,  # noqa
+        sql_zoom="""
+SELECT
+    datetime AS date,
+    dbname,
+    (record).ratio
+FROM %(tablename)s
+WHERE instance_id = %(instance_id)s AND datetime >= %(start)s AND datetime <= %(end)s
+ORDER BY 1, 2
+        """,  # noqa
+        probename='btree_bloat',
+        pivot=dict(
+            index='date',
+            key='dbname',
+            value='ratio',
+        )
+    ),
 )
 
 
