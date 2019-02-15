dalibo/temboard

commit 551ee3f52f56c45b05c7e715099029a147de0d2d

pgconf: Drop t.w.asynchronous from /configuration

temboardui/plugins/pgconf/__init__.py
@@ -1,28 +1,21 @@
+import logging
 from os import path
 import tornado.web
-import tornado.escape
+from tornado.escape import url_escape, url_unescape
 
-from temboardui.handlers.base import BaseHandler
-from temboardui.temboardclient import (
-    TemboardError,
-    temboard_get_configuration,
-    temboard_get_configuration_categories,
-    temboard_get_configuration_status,
-    temboard_post_configuration,
-    temboard_profile,
-)
-from temboardui.async import (
-    HTMLAsyncResult,
-    run_background,
-)
-from temboardui.errors import TemboardUIError
 from temboardui.web import (
     Blueprint,
+    HTTPError,
+    Redirect,
+    TemplateRenderer,
 )
 
 
+logger = logging.getLogger(__name__)
 blueprint = Blueprint()
 blueprint.generic_proxy("/pgconf/configuration", methods=["POST"])
+plugin_path = path.dirname(path.realpath(__file__))
+render_template = TemplateRenderer(plugin_path + "/templates")
 
 
 def configuration(config):
@@ -30,22 +23,7 @@ def configuration(config):
 
 
 def get_routes(config):
-    plugin_path = path.dirname(path.realpath(__file__))
-    handler_conf = {
-        'ssl_ca_cert_file': config.temboard['ssl_ca_cert_file'],
-        'template_path':  plugin_path + "/templates"
-    }
     routes = blueprint.rules + [
-        (
-            r"/server/(.*)/([0-9]{1,5})/pgconf/configuration$",
-            ConfigurationHandler,
-            handler_conf
-        ),
-        (
-            r"/server/(.*)/([0-9]{1,5})/pgconf/configuration/category/(.+)$",
-            ConfigurationHandler,
-            handler_conf
-        ),
         (
             r"/js/pgconf/(.*)",
             tornado.web.StaticFileHandler,
@@ -60,160 +38,58 @@ def get_routes(config):
     return routes
 
 
-class ConfigurationHandler(BaseHandler):
-    """  HTML handler """
+@blueprint.instance_route("/pgconf/configuration(?:/category/(.+))?",
+                          methods=["GET", "POST"])
+def configuration_handler(request, category=None):
+    request.instance.check_active_plugin(__name__)
+    profile = request.instance.get_profile()
+    agent_username = profile['username']
+    template_vars = {}
+    # Deduplicate HTTP prefix of plugin on agent.
+    prefix = "/pgconf/configuration"
+    query_filter = request.handler.get_argument('filter', None, strip=True)
 
-    @BaseHandler.catch_errors
-    def get_configuration(self, agent_address, agent_port, category=None):
-        self.logger.info("Getting configuration.")
-
-        self.setUp(agent_address, agent_port)
-        self.check_active_plugin(__name__)
-
-        xsession = self.get_secure_cookie(
-            "temboard_%s_%s" %
-            (agent_address, agent_port))
-        if not xsession:
-            raise TemboardUIError(401, "Authentication cookie is missing.")
+    if "GET" == request.method:
+        status = request.instance.get(prefix + "/status")
+        categories = request.instance.get(prefix + "/categories")
+        logger.debug("category=%s", category)
+        if category:
+            category = url_unescape(category)
         else:
-            data_profile = temboard_profile(self.ssl_ca_cert_file,
-                                            agent_address,
-                                            agent_port,
-                                            xsession)
-            agent_username = data_profile['username']
+            category = categories['categories'][0]
 
-        configuration_status = temboard_get_configuration_status(
-                                    self.ssl_ca_cert_file,
-                                    agent_address,
-                                    agent_port,
-                                    xsession)
-        configuration_cat = temboard_get_configuration_categories(
-                                    self.ssl_ca_cert_file,
-                                    agent_address,
-                                    agent_port,
-                                    xsession)
-        query_filter = self.get_argument('filter', None, True)
-        if category is None:
-            category = tornado.escape.url_escape(
-                configuration_cat['categories'][0])
-        url = tornado.escape.url_escape(
-            tornado.escape.url_unescape(category))
-        configuration_data = temboard_get_configuration(
-            self.ssl_ca_cert_file,
-            agent_address,
-            agent_port,
-            xsession,
-            url,
-            query_filter)
-        self.logger.info("Done.")
-
-        return HTMLAsyncResult(
-                http_code=200,
-                template_path=self.template_path,
-                template_file='configuration.html',
-                data={
-                    'nav': True,
-                    'role': self.current_user,
-                    'instance': self.instance,
-                    'plugin': __name__,
-                    'data': configuration_data,
-                    'xsession': xsession,
-                    'agent_username': agent_username,
-                    'current_cat': tornado.escape.url_unescape(category),
-                    'configuration_categories': configuration_cat,
-                    'configuration_status': configuration_status,
-                    'query_filter': query_filter
-                })
-
-    @tornado.web.asynchronous
-    def get(self, agent_address, agent_port, category=None):
-        run_background(self.get_configuration, self.async_callback,
-                       (agent_address, agent_port, category))
-
-    @BaseHandler.catch_errors
-    def post_configuration(self, agent_address, agent_port, category=None):
-        self.logger.info("Posting configuration.")
-
-        self.setUp(agent_address, agent_port)
-        self.check_active_plugin(__name__)
-
-        xsession = self.get_secure_cookie(
-            "temboard_%s_%s" %
-            (agent_address, agent_port))
-        if not xsession:
-            raise TemboardUIError(401, "Authentication cookie is missing.")
-
-        query_filter = self.get_argument('filter', None, True)
-        error_code = None
-        error_message = None
-        post_settings = self.request.arguments
-        ret_post = None
-        settings = {'settings': []}
-        for setting_name, setting_value in post_settings.iteritems():
+        logger.debug("category=%s", category)
+        configuration_url = prefix + "/category/" + url_escape(category)
+        query = {'filter': query_filter} if query_filter else {}
+        configuration = request.instance.get(configuration_url, query=query)
+    else:
+        settings = {'settings': [
+            {'name': name, 'setting': value[0]}
+            for name, value in request.arguments.iteritems()
             # 'filter' is not a setting, just ignore it.
-            if setting_name == 'filter':
-                continue
-            settings['settings'].append({'name': setting_name,
-                                         'setting': setting_value[0]})
+            if name != 'filter'
+        ]}
         try:
-            # Try to send settings to the agent.
-            ret_post = temboard_post_configuration(
-                            self.ssl_ca_cert_file,
-                            agent_address,
-                            agent_port,
-                            xsession,
-                            settings)
-        except TemboardError as e:
-            error_code = e.code
-            error_message = e.message
-        # Get PostgreSQL configuration status: needs restart or is fine.
-        configuration_status = temboard_get_configuration_status(
-                                    self.ssl_ca_cert_file,
-                                    agent_address,
-                                    agent_port,
-                                    xsession)
-        # Load settings categories.
-        configuration_cat = temboard_get_configuration_categories(
-                                self.ssl_ca_cert_file,
-                                agent_address,
-                                agent_port,
-                                xsession)
-        if category is None:
-            category = tornado.escape.url_escape(
-                configuration_cat['categories'][0])
-        # Load settings depending on the current category or the filter
-        # value.
-        url = tornado.escape.url_escape(
-            tornado.escape.url_unescape(category))
-        configuration_data = temboard_get_configuration(
-                                self.ssl_ca_cert_file,
-                                agent_address,
-                                agent_port,
-                                xsession,
-                                url,
-                                query_filter)
-        self.logger.info("Done.")
-        return HTMLAsyncResult(
-                http_code=200,
-                template_path=self.template_path,
-                template_file='configuration.html',
-                data={
-                    'nav': True,
-                    'role': self.current_user,
-                    'instance': self.instance,
-                    'plugin': __name__,
-                    'data': configuration_data,
-                    'xsession': xsession,
-                    'current_cat': tornado.escape.url_unescape(category),
-                    'configuration_categories': configuration_cat,
-                    'configuration_status': configuration_status,
-                    'error_code': error_code,
-                    'error_message': error_message,
-                    'ret_post': ret_post,
-                    'query_filter': query_filter
-                })
+            request.instance.post(prefix, body=settings)
+            # Redirect to GET page, same URI.
+            return Redirect(request.uri)
+        except HTTPError as e:
+            # Rerender HTML page with errors.
+            template_vars['error_code'] = e
+            template_vars['error_message'] = e.log_message
 
-    @tornado.web.asynchronous
-    def post(self, agent_address, agent_port, category=None):
-        run_background(self.post_configuration, self.async_callback,
-                       (agent_address, agent_port, category))
+    return render_template(
+        'configuration.html',
+        nav=True,
+        role=request.current_user,
+        instance=request.instance,
+        agent_username=agent_username,
+        plugin=__name__,
+        xsession=request.instance.xsession,
+        current_cat=category,
+        configuration_categories=categories,
+        configuration_status=status,
+        data=configuration,
+        query_filter=query_filter,
+        **template_vars
+    )

temboardui/temboardclient.py
@@ -1,6 +1,5 @@
 import httplib
 import urllib2
-from urllib import quote
 import ssl
 import socket
 import json
@@ -86,90 +85,6 @@ class TemboardError(Exception):
         Exception.__init__(self, message)
         self.code = code
         self.message = message
-
-
-def temboard_get_configuration(in_ca_cert_file,
-                               hostname,
-                               port,
-                               xsession,
-                               enc_category=None,
-                               query_filter=None):
-    try:
-        if query_filter:
-            path = "/pgconf/configuration?filter=" + quote(query_filter)
-        elif enc_category:
-            path = "/pgconf/configuration/category/" + enc_category
-        else:
-            path = "/pgconf/configuration"
-        res = temboard_request(
-            in_ca_cert_file,
-            method='GET',
-            url='https://%s:%s%s' % (hostname, port, path),
-            headers={
-                "Content-type": "application/json",
-                "X-Session": xsession
-            })
-        return json.loads(res)
-    except urllib2.HTTPError as e:
-        raise TemboardError(e.code, json.loads(e.read())['error'])
-    except Exception as e:
-        raise TemboardError(500, str(e))
-
-
-def temboard_post_configuration(in_ca_cert_file, hostname, port, xsession,
-                                settings):
-    try:
-        res = temboard_request(
-            in_ca_cert_file,
-            method='POST',
-            url='https://%s:%s/pgconf/configuration' % (hostname, port),
-            headers={
-                "Content-type": "application/json",
-                "X-Session": xsession
-            },
-            data=settings)
-        return json.loads(res)
-    except urllib2.HTTPError as e:
-        raise TemboardError(e.code, json.loads(e.read())['error'])
-    except Exception as e:
-        raise TemboardError(500, str(e))
-
-
-def temboard_get_configuration_categories(in_ca_cert_file, hostname, port,
-                                          xsession):
-    try:
-        res = temboard_request(
-            in_ca_cert_file,
-            method='GET',
-            url='https://%s:%s/pgconf/configuration/categories' % (hostname,
-                                                                   port),
-            headers={
-                "Content-type": "application/json",
-                "X-Session": xsession
-            })
-        return json.loads(res)
-    except urllib2.HTTPError as e:
-        raise TemboardError(e.code, json.loads(e.read())['error'])
-    except Exception as e:
-        raise TemboardError(500, str(e))
-
-
-def temboard_get_configuration_status(in_ca_cert_file, hostname, port,
-                                      xsession):
-    try:
-        res = temboard_request(
-            in_ca_cert_file,
-            method='GET',
-            url='https://%s:%s/pgconf/configuration/status' % (hostname, port),
-            headers={
-                "Content-type": "application/json",
-                "X-Session": xsession
-            })
-        return json.loads(res)
-    except urllib2.HTTPError as e:
-        raise TemboardError(e.code, json.loads(e.read())['error'])
-    except Exception as e:
-        raise TemboardError(500, str(e))
 
 
 def temboard_discover(in_ca_cert_file, hostname, port):

commit 7631d8dad4e226c376ee76353e78c114563bf4be

Query filter takes precedence over category
Broken in 551ee3f52f56c45b05c7e715099029a147de0d2d

temboardui/plugins/pgconf/__init__.py
@@ -52,15 +52,18 @@ def configuration_handler(request, category=None):
     if "GET" == request.method:
         status = request.instance.get(prefix + "/status")
         categories = request.instance.get(prefix + "/categories")
-        logger.debug("category=%s", category)
-        if category:
-            category = url_unescape(category)
-        else:
-            category = categories['categories'][0]
 
-        logger.debug("category=%s", category)
-        configuration_url = prefix + "/category/" + url_escape(category)
-        query = {'filter': query_filter} if query_filter else {}
+        if query_filter:
+            query = {'filter': query_filter}
+            configuration_url = prefix
+        else:
+            if category:
+                category = url_unescape(category)
+            else:
+                category = categories['categories'][0]
+            logger.debug("category=%s", category)
+            query = {}
+            configuration_url = prefix + "/category/" + url_escape(category)
         configuration = request.instance.get(configuration_url, query=query)
     else:
         settings = {'settings': [