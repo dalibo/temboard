3c0badcd488b25f6d4aa4921aeaf1c8f2a8ab6cd
Pierre GIRAUD <pierre.giraud@dalibo.com>
2020-03-24 11:36:28 +0100

    Add daterangepicker

--- temboardui/plugins/statements/static/js/temboard.statements.js
+++ temboardui/plugins/statements/static/js/temboard.statements.js
@@ -2,31 +2,66 @@
 $(function() {
   "use strict";
 
-  new Vue({
+  var refreshTimeoutId;
+  var refreshInterval = 60 * 1000;
+
+  var v = new Vue({
     el: '#app',
+    router: new VueRouter(),
     data: {
       statements: [],
       sortBy: 'total_time',
-      filter: ''
+      filter: '',
+      from: null,
+      to: null
     },
     computed: {
-      fields: getFields
-    },
-    created: function() {
-      this.fetchData();
+      fields: getFields,
+      fromTo: function() {
+        return this.from, this.to, new Date();
+      }
     },
     methods: {
-      fetchData: fetchData
+      fetchData: fetchData,
+      onPickerUpdate: onPickerUpdate
+    },
+    watch: {
+      fromTo: function() {
+        this.$router.replace({ query: _.assign({}, this.$route.query, {
+          start: '' + v.from,
+          end: '' + v.to
+        })});
+        this.fetchData();
+      },
     }
   });
 
+  var start = v.$route.query.start || 'now-24h';
+  var end = v.$route.query.end || 'now';
+  v.from = start;
+  v.to = end;
+
   function fetchData() {
+    var startDate = dateMath.parse(this.from);
+    var endDate = dateMath.parse(this.to, true);
+
     $.ajax({
       url: apiUrl,
+      data: {
+        start: timestampToIsoDate(startDate),
+        end: timestampToIsoDate(endDate),
+        noerror: 1
+      },
       contentType: "application/json",
       success: (function(data) {
         this.statements = data.data;
         window.setTimeout(postCreated, 1);
+
+        window.clearTimeout(refreshTimeoutId);
+        if (this.from.toString().indexOf('now') != -1 ||
+            this.to.toString().indexOf('now') != -1) {
+          refreshTimeoutId = window.setTimeout(fetchData.bind(this), refreshInterval);
+        }
       }).bind(this)
     });
   }
@@ -73,5 +108,15 @@ $(function() {
     $('.sql').each(function(i, block) {
       hljs.highlightBlock(block);
     });
+  }
+
+  function onPickerUpdate(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+
+  function timestampToIsoDate(epochMs) {
+    var ndate = new Date(epochMs);
+    return ndate.toISOString();
   }
 });
