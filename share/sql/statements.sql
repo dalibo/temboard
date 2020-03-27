8b8b07d9d9aa61f2312b117487d27741c95d4fda
Pierre GIRAUD <pierre.giraud@dalibo.com>
2020-03-27 14:31:49 +0100

    Load data from agent and display it
    Data is pulled from agent using secret_key.
    The pull implementation here is temporary and may be revised later to get
    something more 'centralized'.
    
    A new encoder for JSON is used to handle datetime.
    Data displayed is the aggregation of statements stored for the given
    time range.

---
+++ share/sql/statements.sql
@@ -0,0 +1,44 @@
+DROP SCHEMA IF EXISTS statements CASCADE;
+CREATE SCHEMA statements;
+SET search_path TO statements, public;
+
+
+BEGIN;
+
+CREATE TYPE statement_record AS (
+  blk_read_time DOUBLE PRECISION,
+  blk_write_time DOUBLE PRECISION,
+  calls BIGINT,
+  datname TEXT,
+  dbid INTEGER,
+  local_blks_hit BIGINT,
+  local_blks_read BIGINT,
+  local_blks_dirtied BIGINT,
+  local_blks_written BIGINT,
+  max_time DOUBLE PRECISION,
+  mean_time DOUBLE PRECISION,
+  min_time DOUBLE PRECISION,
+  query TEXT,
+  queryid BIGINT,
+  rolname TEXT,
+  rows BIGINT,
+  shared_blks_dirtied BIGINT,
+  shared_blks_hit BIGINT,
+  shared_blks_read BIGINT,
+  shared_blks_written BIGINT,
+  stddev_time DOUBLE PRECISION,
+  temp_blks_read BIGINT,
+  temp_blks_written BIGINT,
+  total_time DOUBLE PRECISION,
+  userid INTEGER
+);
+
+CREATE TABLE statements_history_current (
+  datetime TIMESTAMPTZ NOT NULL,
+  agent_address TEXT NOT NULL,
+  agent_port INTEGER NOT NULL,
+  record statement_record NOT NULL,
+  FOREIGN KEY (agent_address, agent_port) REFERENCES application.instances (agent_address, agent_port) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+COMMIT;
--- temboardui/plugins/statements/__init__.py
+++ temboardui/plugins/statements/__init__.py
@@ -1,6 +1,21 @@
+import logging
 from os import path
 import tornado.web
-import random
+import urllib2
+from tornado.escape import json_decode
+from tornado.web import (
+    HTTPError,
+)
+
+from sqlalchemy.orm import (
+    sessionmaker,
+    scoped_session,
+)
+from sqlalchemy import create_engine
+from temboardui.model.orm import (
+    Instances,
+)
+from temboardui.errors import TemboardUIError
 
 from temboardui.web import (
     Blueprint,
@@ -11,6 +26,14 @@ from temboardui.plugins.monitoring.tools import (
     get_request_ids,
     parse_start_end,
 )
+from temboardui.toolkit import taskmanager
+from temboardui.temboardclient import (
+    temboard_request,
+)
+
+
+logger = logging.getLogger(__name__)
+workers = taskmanager.WorkerSet()
 
 PLUGIN_NAME = 'statements'
 
@@ -45,40 +68,48 @@ def data(request):
     host_id, instance_id = get_request_ids(request)
     start, end = parse_start_end(request)
 
-    # fake random data
-    data = {
-            "data": [
-                {
-                    "username": "postgres",
-                    "dbname": random.choice(["bench", "tpc", "database"]),
-                    "queryid": random.randint(0, 10000000),
-                    "query": random.choice(["SELECT pg_sleep($1)",
-                                            "SELECT count(*) FROM mytable",
-                                            "SELECT count(*) FROM commandes cmd JOIN lignes_commandes lc ON lc.numero_commande = cmd.numero_commande WHERE cmd.client_id = $1"]), # noqa
-                    "calls": random.choice(range(1000)),
-                    "total_time": random.random() * 5000,
-                    "min_time": 1001.583008,
-                    "max_time": 1001.583008,
-                    "mean_time": 1001.583008,
-                    "stddev_time": 0,
-                    "rows": 1,
-                    "shared_blks_hit": 0,
-                    "shared_blks_read": 0,
-                    "shared_blks_dirtied": 0,
-                    "shared_blks_written": 0,
-                    "local_blks_hit": 0,
-                    "local_blks_read": 0,
-                    "local_blks_dirtied": 0,
-                    "local_blks_written": 0,
-                    "temp_blks_read ": 0,
-                    "temp_blks_written": 0,
-                    "blk_read_time": 0,
-                    "blk_write_time": 0
-                }
-                for i in range(10)]
-            }
-
-    return jsonify(data=data)
+    query = """
+        SELECT
+          sum((record).blk_read_time) AS blk_read_time,
+          sum((record).blk_write_time) AS blk_write_time,
+          sum((record).calls) AS calls,
+          (record).datname,
+          sum((record).local_blks_dirtied) AS local_blks_dirtied,
+          sum((record).local_blks_hit) AS local_blks_hit,
+          sum((record).local_blks_read) AS local_blks_read,
+          sum((record).local_blks_written) AS local_blks_written,
+          max((record).max_time) AS max_time,
+          sum((record).total_time) / sum((record).calls) AS mean_time,
+          min((record).min_time) AS min_time,
+          (record).query,
+          (record).rolname,
+          sum((record).rows) AS rows,
+          sum((record).shared_blks_dirtied) AS shared_blks_dirtied,
+          sum((record).shared_blks_hit) AS shared_blks_hit,
+          sum((record).shared_blks_read) AS shared_blks_read,
+          sum((record).shared_blks_written) AS shared_blks_written,
+          sum((record).stddev_time) AS stddev_time,
+          sum((record).temp_blks_read) AS temp_blks_read,
+          sum((record).temp_blks_written) AS temp_blks_written,
+          sum((record).total_time) AS total_time
+        FROM statements.statements_history_current
+        WHERE agent_address = :agent_address
+        AND agent_port = :agent_port
+        AND datetime <@ tstzrange(:start, :end, '[]')
+        GROUP BY (record).queryid,
+                 (record).query,
+                 (record).datname,
+                 (record).rolname
+    """
+    statements = request.db_session.execute(
+        query,
+        dict(agent_address=request.instance.agent_address,
+             agent_port=request.instance.agent_port,
+             start=start,
+             end=end)) \
+        .fetchall()
+    statements = [dict(statement) for statement in statements]
+    return jsonify(dict(data=statements))
 
 
 @blueprint.instance_route(r'/statements')
@@ -91,4 +122,129 @@ def statements(request):
         instance=request.instance,
         plugin=__name__,
         role=request.current_user,
+    )
+
+
+def add_statement(session, instance, data):
+    try:
+        for statement in data.get('data'):
+            cur = session.connection().connection.cursor()
+            query = """
+                INSERT INTO statements.statements_history_current
+                VALUES (%s, %s, %s, %s)
+            """
+            cur.execute(
+                query,
+                (
+                    data['snapshot_datetime'],
+                    instance.agent_address,
+                    instance.agent_port,
+                    (
+                        statement['blk_read_time'],
+                        statement['blk_write_time'],
+                        statement['calls'],
+                        statement['datname'],
+                        statement['dbid'],
+                        statement['local_blks_dirtied'],
+                        statement['local_blks_hit'],
+                        statement['local_blks_read'],
+                        statement['local_blks_written'],
+                        statement['max_time'],
+                        statement['mean_time'],
+                        statement['min_time'],
+                        statement['query'],
+                        statement['queryid'],
+                        statement['rolname'],
+                        statement['rows'],
+                        statement['shared_blks_dirtied'],
+                        statement['shared_blks_hit'],
+                        statement['shared_blks_read'],
+                        statement['shared_blks_written'],
+                        statement['stddev_time'],
+                        statement['temp_blks_read'],
+                        statement['temp_blks_written'],
+                        statement['total_time'],
+                        statement['userid'],
+                    )
+                )
+            )
+        session.connection().connection.commit()
+    except Exception as e:
+        raise TemboardUIError(400, e.message)
+
+
+@workers.register(pool_size=1)
+def pull_data_worker(app):
+    # Worker in charge of retrieving statements data
+    dbconf = app.config.repository
+    dburi = 'postgresql://{user}:{pwd}@:{p}/{db}?host={h}'.format(
+                user=dbconf['user'],
+                pwd=dbconf['password'],
+                h=dbconf['host'],
+                p=dbconf['port'],
+                db=dbconf['dbname']
+            )
+    engine = create_engine(dburi)
+    session_factory = sessionmaker(bind=engine)
+    Session = scoped_session(session_factory)
+
+    worker_session = Session()
+    instances = worker_session.query(Instances)
+
+    for instance in instances:
+        plugin_names = [plugin.plugin_name for plugin in instance.plugins]
+
+        if 'statements' not in plugin_names:
+            continue
+
+        # FIXME should be done in parallel
+        try:
+            pull_data_for_instance(app, worker_session, instance)
+        except Exception:
+            pass
+
+
+def pull_data_for_instance(app, session, instance):
+    try:
+        url = 'https://%s:%s%s?key=%s' % (
+            instance.agent_address,
+            instance.agent_port,
+            '/statements',
+            instance.agent_key,
+        )
+        headers = {}
+        try:
+            body = temboard_request(
+                app.config.temboard.ssl_ca_cert_file,
+                method='GET',
+                url=url,
+                headers=headers,
+            )
+        except urllib2.HTTPError as e:
+            message = e.read()
+            try:
+                message = json_decode(message)['error']
+            except Exception as ee:
+                logger.debug("Failed to decode agent error: %s.", ee)
+            raise HTTPError(e.code, message)
+        except urllib2.URLError as e:
+            logger.error("Proxied request failed: %s", e)
+            raise HTTPError(500, str(e.reason))
+        except Exception as e:
+            logger.error("Proxied request failed: %s", e)
+            raise HTTPError(500)
+        add_statement(session, instance, json_decode(body))
+    except Exception as e:
+        logger.error('Could not get statements data')
+        logger.exception(e)
+        raise(e)
+
+
+@taskmanager.bootstrap()
+def statements_bootstrap(context):
+    yield taskmanager.Task(
+        worker_name='pull_data_worker',
+        id='statementsdata',
+        redo_interval=1 * 60,  # Repeat each 1m,
+        options={},
     )
