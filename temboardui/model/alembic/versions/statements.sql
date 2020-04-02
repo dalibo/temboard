aa62a4603c9d2089df560608bede65cfbca5012d
Denis Laxalde <denis.laxalde@dalibo.com>
2020-04-02 10:42:37 +0200

    Extract statements's metadata into a separate table
    We create a "statement" table to store metadata received from the agent
    by request on the /statements endpoint in a new "statements" table. The
    statements_history_current now references the multi-column primary key
    of this table.
    
    This should help limiting the storage volume by avoiding to store the
    same query multiple times and makes it easier to add indexes.

--- temboardui/model/alembic/versions/statements.sql
+++ temboardui/model/alembic/versions/statements.sql
@@ -5,12 +5,23 @@ SET search_path TO statements, public;
 
 BEGIN;
 
+CREATE TABLE statements (
+  agent_address TEXT NOT NULL,
+  agent_port INTEGER NOT NULL,
+  queryid BIGINT NOT NULL,
+  query TEXT NOT NULL,
+  dbid INTEGER NOT NULL,
+  datname TEXT NOT NULL,
+  userid INTEGER NOT NULL,
+  rolname TEXT NOT NULL,
+  FOREIGN KEY (agent_address, agent_port) REFERENCES application.instances (agent_address, agent_port) ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (agent_address, agent_port, queryid, dbid, userid)
+);
+
 CREATE TYPE statement_record AS (
   blk_read_time DOUBLE PRECISION,
   blk_write_time DOUBLE PRECISION,
   calls BIGINT,
-  datname TEXT,
-  dbid INTEGER,
   local_blks_hit BIGINT,
   local_blks_read BIGINT,
   local_blks_dirtied BIGINT,
@@ -18,9 +29,6 @@ CREATE TYPE statement_record AS (
   max_time DOUBLE PRECISION,
   mean_time DOUBLE PRECISION,
   min_time DOUBLE PRECISION,
-  query TEXT,
-  queryid BIGINT,
-  rolname TEXT,
   rows BIGINT,
   shared_blks_hit BIGINT,
   shared_blks_read BIGINT,
@@ -29,16 +37,18 @@ CREATE TYPE statement_record AS (
   stddev_time DOUBLE PRECISION,
   temp_blks_read BIGINT,
   temp_blks_written BIGINT,
-  total_time DOUBLE PRECISION,
-  userid INTEGER
+  total_time DOUBLE PRECISION
 );
 
 CREATE TABLE statements_history_current (
   datetime TIMESTAMPTZ NOT NULL,
   agent_address TEXT NOT NULL,
   agent_port INTEGER NOT NULL,
+  queryid BIGINT NOT NULL,
+  dbid INTEGER NOT NULL,
+  userid INTEGER NOT NULL,
   record statement_record NOT NULL,
-  FOREIGN KEY (agent_address, agent_port) REFERENCES application.instances (agent_address, agent_port) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (agent_address, agent_port, queryid, dbid, userid) REFERENCES statements ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 COMMIT;
