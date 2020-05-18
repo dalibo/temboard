706e9cfb0fc68056766321b9005457ded6b7a12e
Pierre GIRAUD <pierre.giraud@dalibo.com>
2020-05-18 08:32:25 +0200

    Remove unrequired assertion
    fix for e7a959f4abd5d414130ef776a850ac3b39bdebbd

--- tests/unit/test_plugins_pgconf.py
+++ tests/unit/test_plugins_pgconf.py
@@ -6,6 +6,6 @@ def test_human_to_number():
 
     assert 2 == human_to_number('2ms', 'ms')
     with pytest.raises(Exception):
-        assert 0.2 == human_to_number('0.2ms', 'ms')
+        human_to_number('0.2ms', 'ms')
     assert 0.2 == human_to_number('0.2ms', 'ms', float)
     assert 2.2 == human_to_number('2200us', 'ms')
