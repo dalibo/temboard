16157e3267180dcc0076a35d21581a0e374af79f
Étienne BERSAC <etienne.bersac@dalibo.com>
2020-08-27 17:30:10 +0200

    func: Naïve Python 3 support

--- tests/func/run_tests_docker.sh
+++ tests/func/run_tests_docker.sh
@@ -45,6 +45,7 @@ fi
 
 pip2 install pytest
 
+export LC_ALL=en_US.utf8
 export TBD_PGBIN=$(readlink -e /usr/pgsql-${TBD_PGVERSION}/bin /usr/lib/postgresql/${TBD_PGVERSION}/bin)
 
 # Remove any .pyc file to avoid errors with pytest and cache
