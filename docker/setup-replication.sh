293891ef853c71f3e15438b8697edca645936efc
Étienne BERSAC <etienne.bersac@dalibo.com>
2020-10-13 15:22:45 +0200

    docker: Setup replication for Postgres 9.6

--- docker/setup-replication.sh
+++ docker/setup-replication.sh
@@ -1,21 +1,56 @@
+# -*- mode: sh; sh-shell: bash -*-
+#
+# This script setups both primary and secondary docker service for streaming
+# physical replication.
+#
+# Simply mount this script in /docker-init-entrypoint.d/ and define
+# `PRIMARY_HOST` env var in secondary, pointing to primary service name.
+#
+# Tested with Postgres 13 and 9.6, excluding any other version. Easy test with:
+#
+# docker-compose down -v ; docker-compose up primary secondary
+#
+
+primary_psql() {
+	psql -h "$PRIMARY_HOST" "$@"
+}
+
+check_primary_is_ready() {
+	local query
+
+	if initdb --version | grep -qF ' 9.' ; then
+		query="SELECT DISTINCT 'SUCCESS' FROM pg_settings WHERE name = 'wal_level' AND setting = 'replica';"
+	else
+		query="SELECT DISTINCT 'SUCCESS' FROM pg_hba_file_rules WHERE database::TEXT = '{replication}' AND address IS NULL;"
+	fi
+
+	primary_psql -c "$query" | grep -q SUCCESS
+}
+
 if [ -z "${PRIMARY_HOST-}" ]; then
 	cat >> "${PGDATA}/postgresql.conf" <<-EOF
-	wal_level = hot_standby
+	wal_level = replica
 	max_wal_senders = 8
-	hot_standby = on
+	max_replication_slots = 8
 	EOF
 	cat >> "$PGDATA/pg_hba.conf" <<-EOF
 	host    replication     all             all            ${POSTGRES_HOST_AUTH_METHOD}
 	EOF
 	pg_ctl -D "$PGDATA" -w reload
 else
-	for j in {0..5} ; do
-		if psql -h "$PRIMARY_HOST" -c "SELECT DISTINCT 'SUCCESS' FROM pg_hba_file_rules WHERE database::TEXT = '{replication}' AND address IS NULL;" | grep -q SUCCESS ; then
+	for i in {0..9} ; do
+		if check_primary_is_ready ; then
 			break
+		elif [ $i -lt 6 ] ; then
+			sleep $i
 		else
-			sleep $j
+			exit 1
 		fi
 	done
+
+	# Avoid using --create-slot not in PostgreSQL 9.6
+	slot="replication_$HOSTNAME"
+	primary_psql -tc "SELECT pg_create_physical_replication_slot('$slot');"
 
 	docker_temp_server_stop
 	rm -rf "${PGDATA:?}"/*
@@ -23,16 +58,17 @@ else
 		--verbose \
 		--host="${PRIMARY_HOST}" \
 		--username="${POSTGRES_USER}" \
-		--wal-method=stream \
+		-X stream \
 		--checkpoint=fast \
 		--pgdata="$PGDATA" \
-		--create-slot --slot="replication_$HOSTNAME" \
+		--slot="$slot" \
 		--write-recovery-conf \
 	;
 
-	cat >> "${PGDATA}/postgresql.conf" <<-EOF
-	primary_conninfo = 'host=${PRIMARY_HOST} port=5432 user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}'
+	cat >> "$PGDATA"/postgresql.conf <<-EOF
+	hot_standby = on
 	EOF
 
 	docker_temp_server_start
+	echo "Secondary configured."
 fi
