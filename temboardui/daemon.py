5afb0870883f4aabd11413235f3b8c7a887b12f4
Pierre GIRAUD <pierre.giraud@dalibo.com>
2021-05-12 15:50:45 +0200

    Replace PY2 file() by open()

--- temboardui/daemon.py
+++ temboardui/daemon.py
@@ -75,9 +75,10 @@ def daemonize(pidfile, config):
     # Redirect standard file descriptors.
     sys.stdout.flush()
     sys.stderr.flush()
-    si = file('/dev/null', 'r')
-    so = file('/dev/null', 'a+')
-    se = file('/dev/null', 'a+', 0)
+    si = open(os.devnull, 'r')
+    so = open(os.devnull, 'a+')
+    se = open(os.devnull, 'a+')
+
     os.dup2(si.fileno(), sys.stdin.fileno())
     os.dup2(so.fileno(), sys.stdout.fileno())
     os.dup2(se.fileno(), sys.stderr.fileno())
