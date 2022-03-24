748a7f500e2977b0c5a147a3e236fe9b318ea756
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-03-24 18:41:21 +0100

    dev: Keep agent egg-info owned by dev

--- docker/dev-agent-entrypoint.sh
+++ docker/dev-agent-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eux
 
 pip install -e /usr/local/src/temboard/agent/ psycopg2-binary hupper
+chown -R "$(stat -c "%u:%g" "$0")" /usr/local/src/temboard/agent
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
