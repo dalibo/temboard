b644036b51762004fdae628f0c3ae1f7a84d2a6f
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-05-17 11:50:05 +0200

    dev: Enable pip cache for agents

--- dev/agent-entrypoint.sh
+++ dev/agent-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -eux
 
+export PIP_CACHE_DIR=/usr/local/src/temboard/dev/pip-cache
+mkdir -p $PIP_CACHE_DIR
+chown "$(id -u):$(id -g)" $PIP_CACHE_DIR
+pip cache dir
+
 pip install -e /usr/local/src/temboard/agent/ psycopg2-binary hupper
 chown -R "$(stat -c "%u:%g" "$0")" /usr/local/src/temboard/agent
 
