b4cb183bb7274729517eb30270c5dcb0126aec6d
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-05-17 12:10:32 +0200

    dev: Use local agent entrypoint.sh

--- dev/agent-entrypoint.sh
+++ dev/agent-entrypoint.sh
@@ -8,4 +8,4 @@ pip cache dir
 pip install -e /usr/local/src/temboard/agent/ psycopg2-binary hupper
 chown -R "$(stat -c "%u:%g" "$0")" /usr/local/src/temboard/agent
 
-exec /usr/local/bin/docker-entrypoint.sh "$@"
+exec /usr/local/src/temboard/agent/docker/entrypoint.sh "$@"
