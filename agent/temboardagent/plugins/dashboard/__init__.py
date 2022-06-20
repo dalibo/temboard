abad424edf5fe80b08f57090fbdde9ba31aad0f4
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-06-20 16:29:16 +0200

    agent: Move Dashboard to Bottle

--- agent/temboardagent/plugins/dashboard/__init__.py
+++ agent/temboardagent/plugins/dashboard/__init__.py
@@ -1,111 +1,107 @@
 import logging
 import time
 
+from bottle import Bottle, default_app
+
 from ...toolkit import taskmanager
 from ...toolkit.configuration import OptionSpec
-from ...routing import RouteSet
 
 from . import db
 from . import metrics
 
+
+bottle = Bottle()
 logger = logging.getLogger(__name__)
-routes = RouteSet(prefix=b'/dashboard')
 workers = taskmanager.WorkerSet()
 
 
-@routes.get(b'')
-def dashboard(http_context, app):
-    return metrics.get_metrics_queue(app.config)
+@bottle.get('/')
+def dashboard():
+    return metrics.get_metrics_queue(default_app().temboard.config)
 
 
-@routes.get(b'/config')
-def dashboard_config(http_context, app):
+@bottle.get('/config')
+def dashboard_config():
+    app = default_app().temboard
     return dict(
         scheduler_interval=app.config.dashboard.scheduler_interval,
         history_length=app.config.dashboard.history_length,
     )
 
 
-@routes.get(b'/live')
-def dashboard_live(http_context, app):
-    return metrics.get_metrics(app)
+@bottle.get('/live')
+def dashboard_live():
+    return metrics.get_metrics(default_app().temboard)
 
 
-@routes.get(b'/history')
-def dashboard_history(http_context, app):
-    return metrics.get_history_metrics_queue(app.config)
+@bottle.get('/history')
+def dashboard_history():
+    return metrics.get_history_metrics_queue(default_app().temboard.config)
 
 
-@routes.get(b'/buffers')
-def dashboard_buffers(http_context, app):
-    with app.postgres.connect() as conn:
-        return metrics.get_buffers(conn)
+@bottle.get('/buffers')
+def dashboard_buffers(pgconn):
+    return metrics.get_buffers(pgconn)
 
 
-@routes.get(b'/hitratio')
-def dashboard_hitratio(http_context, app):
-    with app.postgres.connect() as conn:
-        return metrics.get_hitratio(conn)
+@bottle.get('/hitratio')
+def dashboard_hitratio(pgconn):
+    return metrics.get_hitratio(pgconn)
 
 
-@routes.get(b'/active_backends')
-def dashboard_active_backends(http_context, app):
-    with app.postgres.connect() as conn:
-        return metrics.get_active_backends(conn)
+@bottle.get('/active_backends')
+def dashboard_active_backends(pgconn):
+    return metrics.get_active_backends(pgconn)
 
 
-@routes.get(b'/cpu')
-def dashboard_cpu(http_context, app):
+@bottle.get('/cpu')
+def dashboard_cpu():
     return metrics.get_cpu_usage()
 
 
-@routes.get(b'/loadaverage')
-def dashboard_loadaverage(http_context, app):
+@bottle.get('/loadaverage')
+def dashboard_loadaverage():
     return metrics.get_loadaverage()
 
 
-@routes.get(b'/memory')
-def dashboard_memory(http_context, app):
+@bottle.get('/memory')
+def dashboard_memory():
     return metrics.get_memory_usage()
 
 
-@routes.get(b'/hostname')
-def dashboard_hostname(http_context, app):
-    return metrics.get_hostname(app.config)
+@bottle.get('/hostname')
+def dashboard_hostname():
+    return metrics.get_hostname(default_app().temboard.config)
 
 
-@routes.get(b'/os_version')
-def dashboard_os_version(http_context, app):
+@bottle.get('/os_version')
+def dashboard_os_version():
     return metrics.get_os_version()
 
 
-@routes.get(b'/pg_version')
-def dashboard_pg_version(http_context, app):
-    with app.postgres.connect() as conn:
-        return metrics.get_pg_version(conn)
+@bottle.get('/pg_version')
+def dashboard_pg_version(pgconn):
+    return metrics.get_pg_version(pgconn)
 
 
-@routes.get(b'/n_cpu')
-def dashboard_n_cpu(http_context, app):
+@bottle.get('/n_cpu')
+def dashboard_n_cpu():
     return metrics.get_n_cpu()
 
 
-@routes.get(b'/databases')
-def dashboard_databases(http_context, app):
-    with app.postgres.connect() as conn:
-        return metrics.get_databases(conn)
+@bottle.get('/databases')
+def dashboard_databases(pgconn):
+    return metrics.get_databases(pgconn)
 
 
-@routes.get(b'/info')
-def dashboard_info(http_context, app):
-    with app.postgres.connect() as conn:
-        return metrics.get_info(conn, app.config)
+@bottle.get('/info')
+def dashboard_info(pgconn):
+    return metrics.get_info(pgconn, default_app().temboard.config)
 
 
-@routes.get(b'/max_connections')
-def dashboard_max_connections(http_context, app):
-    with app.postgres.connect() as conn:
-        return metrics.get_max_connections(conn)
+@bottle.get('/max_connections')
+def dashboard_max_connections(pgconn):
+    return metrics.get_max_connections(pgconn)
 
 
 @workers.register(pool_size=1)
@@ -146,7 +142,7 @@ class DashboardPlugin:
         db.bootstrap(self.app.config.temboard.home, 'dashboard.db')
 
     def load(self):
-        self.app.router.add(routes)
+        default_app().mount('/dashboard', bottle)
         self.app.worker_pool.add(workers)
         workers.schedule(
             id='dashboard_collector',
@@ -157,5 +153,4 @@ class DashboardPlugin:
     def unload(self):
         self.app.scheduler.remove(workers)
         self.app.worker_pool.remove(workers)
-        self.app.router.remove(routes)
         self.app.config.remove_specs(self.option_specs)
