c92c4d27c71098d3d1b09a069b71ee25bbfc3c4d
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-06-28 09:33:32 +0200

    agent: Move maintenance to Bottle

--- agent/temboardagent/plugins/maintenance/__init__.py
+++ agent/temboardagent/plugins/maintenance/__init__.py
@@ -1,98 +1,76 @@
 from datetime import datetime
 
-from temboardagent.routing import RouteSet
+from bottle import Bottle, default_app, request
+
 from temboardagent.toolkit import taskmanager
 from temboardagent.tools import validate_parameters
-from temboardagent.types import T_OBJECTNAME
 
 from . import functions
 
 
-routes = RouteSet(prefix=b'/maintenance')
+bottle = Bottle()
 workers = taskmanager.WorkerSet()
 
 
-@routes.get(b'')
-def get_instance(http_context, app):
-    with app.postgres.connect() as conn:
-        instance = next(functions.get_instance(conn))
+@bottle.get('/')
+def get_instance(pgconn, pgpool):
+    instance = next(functions.get_instance(pgconn))
 
-    with app.postgres.connect() as conn:
-        rows = functions.get_databases(conn)
+    rows = functions.get_databases(pgconn)
 
     databases = []
     for database in rows:
         database = dict(database)
         # we need to connect with a different database
         dbname = database['datname']
-        with functions.get_postgres(app.config, dbname).connect() as conn:
-            database.update(**functions.get_database(conn))
+        dbconn = pgpool.getconn(dbname)
+        database.update(**functions.get_database(dbconn))
         databases.append(database)
 
     return {'instance': instance, 'databases': databases}
 
 
-T_DATABASE_NAME = T_OBJECTNAME
-T_SCHEMA_NAME = T_OBJECTNAME
-T_TABLE_NAME = T_OBJECTNAME
-T_INDEX_NAME = T_OBJECTNAME
 T_VACUUM_MODE = b'(((^|,)(full|freeze|analyze))+$)'
 T_TIMESTAMP_UTC = b'(^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z$)'
-T_OPERATION_ID = b'(^[0-9a-f]{8}$)'
 
 
-@routes.get(b'/%s' % (T_DATABASE_NAME))
-def get_database(http_context, app):
-    dbname = http_context['urlvars'][0]
-    with functions.get_postgres(app.config, dbname).connect() as conn:
-        database = functions.get_database_size(conn)
-        schemas = functions.get_schemas(conn)
+@bottle.get('/<dbname>')
+def get_database(pgpool, dbname):
+    conn = pgpool.getconn(dbname)
+    database = functions.get_database_size(conn)
+    schemas = functions.get_schemas(conn)
     return dict(database, **{'schemas': schemas})
 
 
-@routes.get(b'/%s/schema/%s' % (T_DATABASE_NAME, T_SCHEMA_NAME))
-def get_schema(http_context, app):
-    dbname = http_context['urlvars'][0]
-    schema = http_context['urlvars'][1]
-    with functions.get_postgres(app.config, dbname).connect() \
-            as conn:
-        tables = functions.get_tables(conn, schema)
-        indexes = functions.get_schema_indexes(conn, schema)
-        schema = functions.get_schema(conn, schema)
+@bottle.get('/<dbname>/schema/<schema>')
+def get_schema(pgpool, dbname, schema):
+    conn = pgpool.getconn(dbname)
+    tables = functions.get_tables(conn, schema)
+    indexes = functions.get_schema_indexes(conn, schema)
+    schema = functions.get_schema(conn, schema)
     return dict(dict(tables, **indexes), **schema)
 
 
-@routes.get(b'/%s/schema/%s/table/%s' % (T_DATABASE_NAME, T_SCHEMA_NAME,
-                                         T_TABLE_NAME))
-def get_table(http_context, app):
-    dbname = http_context['urlvars'][0]
-    schema = http_context['urlvars'][1]
-    table = http_context['urlvars'][2]
-
-    with functions.get_postgres(app.config, dbname).connect() \
-            as conn:
-        ret = functions.get_table(conn, schema, table)
-        ret.update(**functions.get_table_indexes(conn, schema, table))
-        return ret
+@bottle.get('/<dbname>/schema/<schema>/table/<table>')
+def get_table(pgpool, dbname, schema, table):
+    conn = pgpool.getconn(dbname)
+    ret = functions.get_table(conn, schema, table)
+    ret.update(**functions.get_table_indexes(conn, schema, table))
+    return ret
 
 
-@routes.post(b'/%s/vacuum' % T_DATABASE_NAME)
-def post_vacuum_database(http_context, app):
-    dbname = http_context['urlvars'][0]
-    return post_vacuum(app, http_context['post'], dbname)
+@bottle.post('/<dbname>/vacuum')
+def post_vacuum_database(pgpool, dbname):
+    return post_vacuum(pgpool, request.json, dbname)
 
 
-@routes.post(b'/%s/schema/%s/table/%s/vacuum' % (T_DATABASE_NAME,
-                                                 T_SCHEMA_NAME,
-                                                 T_TABLE_NAME))
-def post_vacuum_table(http_context, app):
-    dbname = http_context['urlvars'][0]
-    schema = http_context['urlvars'][1]
-    table = http_context['urlvars'][2]
-    return post_vacuum(app, http_context['post'], dbname, schema, table)
+@bottle.post('/<dbname>/schema/<schema>/table/<table>/vacuum')
+def post_vacuum_table(pgpool, dbname, schema, table):
+    return post_vacuum(pgpool, request.json, dbname, schema, table)
 
 
-def post_vacuum(app, post, dbname, schema=None, table=None):
+def post_vacuum(pgpool, post, dbname, schema=None, table=None):
+    app = default_app().temboard
     # Parameters format validation
     if 'datetime' in post:
         validate_parameters(post, [
@@ -105,37 +83,32 @@ def post_vacuum(app, post, dbname, schema=None, table=None):
         ])
     mode = post.get('mode', '')
 
-    with functions.get_postgres(app.config, dbname).connect() as conn:
-        return functions.schedule_vacuum(conn, dbname, mode, dt, app,
-                                         schema=schema, table=table)
+    return functions.schedule_vacuum(
+        pgpool.getconn(dbname), dbname, mode, dt, app, schema, table)
 
 
-@routes.get(b'/%s/vacuum/scheduled' % (T_DATABASE_NAME))
-def scheduled_vacuum_database(http_context, app):
-    dbname = http_context['urlvars'][0]
+@bottle.get('/<dbname>/vacuum/scheduled')
+def scheduled_vacuum_database(dbname):
+    app = default_app().temboard
     return functions.list_scheduled_vacuum(app, dbname=dbname)
 
 
-@routes.get(b'/%s/schema/%s/table/%s/vacuum/scheduled' % (T_DATABASE_NAME,
-                                                          T_SCHEMA_NAME,
-                                                          T_TABLE_NAME))
-def scheduled_vacuum_table(http_context, app):
-    dbname = http_context['urlvars'][0]
-    schema = http_context['urlvars'][1]
-    table = http_context['urlvars'][2]
+@bottle.get('/<dbname>/schema/<schema>/table/<table>/vacuum/scheduled')
+def scheduled_vacuum_table(dbname, schema, table):
+    app = default_app().temboard
     return functions.list_scheduled_vacuum(app, dbname=dbname, schema=schema,
                                            table=table)
 
 
-@routes.delete(b'/vacuum/' + T_OPERATION_ID)
-def delete_vacuum(http_context, app):
-    vacuum_id = http_context['urlvars'][0]
+@bottle.delete('/vacuum/<vacuum_id>')
+def delete_vacuum(vacuum_id):
+    app = default_app().temboard
     return functions.cancel_scheduled_operation(vacuum_id, app)
 
 
-@routes.get(b'/vacuum/scheduled')
-def scheduled_vacuum(http_context, app):
-    return functions.list_scheduled_vacuum(app)
+@bottle.get('/vacuum/scheduled')
+def scheduled_vacuum():
+    return functions.list_scheduled_vacuum(default_app().temboard)
 
 
 @workers.register(pool_size=10)
@@ -145,23 +118,18 @@ def vacuum_worker(app, dbname, mode, schema=None, table=None):
         return functions.vacuum(conn, dbname, mode, schema, table)
 
 
-@routes.post(b'/%s/analyze' % T_DATABASE_NAME)
-def post_analyze_database(http_context, app):
-    dbname = http_context['urlvars'][0]
-    return post_analyze(app, http_context['post'], dbname)
+@bottle.post('/<dbname>/analyze')
+def post_analyze_database(pgpool, dbname):
+    return post_analyze(pgpool, request.json, dbname)
 
 
-@routes.post(b'/%s/schema/%s/table/%s/analyze' % (T_DATABASE_NAME,
-                                                  T_SCHEMA_NAME,
-                                                  T_TABLE_NAME))
-def post_analyze_table(http_context, app):
-    dbname = http_context['urlvars'][0]
-    schema = http_context['urlvars'][1]
-    table = http_context['urlvars'][2]
-    return post_analyze(app, http_context['post'], dbname, schema, table)
+@bottle.post('/<dbname>/schema/<schema>/table/<table>/analyze')
+def post_analyze_table(pgpool, dbname, schema, table):
+    return post_analyze(pgpool, request.json, dbname, schema, table)
 
 
-def post_analyze(app, post, dbname, schema=None, table=None):
+def post_analyze(pgpool, post, dbname, schema=None, table=None):
+    app = default_app().temboard
     # Parameters format validation
     if 'datetime' in post:
         validate_parameters(post, [
@@ -169,36 +137,32 @@ def post_analyze(app, post, dbname, schema=None, table=None):
         ])
     dt = post.get('datetime', datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"))
 
-    with functions.get_postgres(app.config, dbname).connect() as conn:
-        return functions.schedule_analyze(conn, dbname, dt, app,
-                                          schema=schema, table=table)
+    conn = pgpool.getconn(dbname)
+    return functions.schedule_analyze(conn, dbname, dt, app, schema, table)
 
 
-@routes.get(b'/%s/analyze/scheduled' % (T_DATABASE_NAME))
-def scheduled_analyze_database(http_context, app):
-    dbname = http_context['urlvars'][0]
+@bottle.get('/<dbname>/analyze/scheduled')
+def scheduled_analyze_database(dbname):
+    app = default_app().temboard
     return functions.list_scheduled_analyze(app, dbname=dbname)
 
 
-@routes.get(b'/%s/schema/%s/table/%s/analyze/scheduled' % (T_DATABASE_NAME,
-                                                           T_SCHEMA_NAME,
-                                                           T_TABLE_NAME))
-def scheduled_analyze_table(http_context, app):
-    dbname = http_context['urlvars'][0]
-    schema = http_context['urlvars'][1]
-    table = http_context['urlvars'][2]
+@bottle.get('/<dbname>/schema/<schema>/table/<table>/analyze/scheduled')
+def scheduled_analyze_table(dbname, schema, table):
+    app = default_app().temboard
     return functions.list_scheduled_analyze(app, dbname=dbname, schema=schema,
                                             table=table)
 
 
-@routes.delete(b'/analyze/' + T_OPERATION_ID)
-def delete_analyze(http_context, app):
-    analyze_id = http_context['urlvars'][0]
+@bottle.delete('/analyze/<analyze_id>')
+def delete_analyze(analyze_id):
+    app = default_app().temboard
     return functions.cancel_scheduled_operation(analyze_id, app)
 
 
-@routes.get(b'/analyze/scheduled')
-def scheduled_analyze(http_context, app):
+@bottle.get('/analyze/scheduled')
+def scheduled_analyze():
+    app = default_app().temboard
     return functions.list_scheduled_analyze(app)
 
 
@@ -209,35 +173,23 @@ def analyze_worker(app, dbname, schema=None, table=None):
         return functions.analyze(conn, dbname, schema, table)
 
 
-@routes.post(b'/%s/reindex' % (T_DATABASE_NAME))
-def post_reindex_database(http_context, app):
-    dbname = http_context['urlvars'][0]
-    return post_reindex(app, http_context['post'], dbname)
+@bottle.post('/<dbname>/reindex')
+def post_reindex_database(pgpool, dbname):
+    return post_reindex(pgpool, request.json, dbname)
 
 
-@routes.post(b'/%s/schema/%s/table/%s/reindex' % (T_DATABASE_NAME,
-                                                  T_SCHEMA_NAME,
-                                                  T_TABLE_NAME))
-def post_reindex_table(http_context, app):
-    dbname = http_context['urlvars'][0]
-    schema = http_context['urlvars'][1]
-    table = http_context['urlvars'][2]
-    return post_reindex(app, http_context['post'], dbname, schema=schema,
-                        table=table)
+@bottle.post('/<dbname>/schema/<schema>/table/<table>/reindex')
+def post_reindex_table(pgpool, dbname, schema, table):
+    return post_reindex(pgpool, request.json, dbname, schema, table)
 
 
-@routes.post(b'/%s/schema/%s/index/%s/reindex' % (T_DATABASE_NAME,
-                                                  T_SCHEMA_NAME,
-                                                  T_INDEX_NAME))
-def post_reindex_index(http_context, app):
-    dbname = http_context['urlvars'][0]
-    schema = http_context['urlvars'][1]
-    index = http_context['urlvars'][2]
-    return post_reindex(app, http_context['post'], dbname, schema=schema,
-                        index=index)
+@bottle.post('/<dbname>/schema/<schema>/index/<index>/reindex')
+def post_reindex_index(pgpool, dbname, schema, index):
+    return post_reindex(pgpool, request.json, dbname, schema, index=index)
 
 
-def post_reindex(app, post, dbname, schema=None, table=None, index=None):
+def post_reindex(pgpool, post, dbname, schema=None, table=None, index=None):
+    app = default_app().temboard
     # Parameters format validation
     if 'datetime' in post:
         validate_parameters(post, [
@@ -245,40 +197,36 @@ def post_reindex(app, post, dbname, schema=None, table=None, index=None):
         ])
     dt = post.get('datetime', datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"))
 
-    with functions.get_postgres(app.config, dbname).connect() as conn:
-        return functions.schedule_reindex(
-            conn, dbname, dt, app, schema=schema, table=table, index=index)
+    return functions.schedule_reindex(
+        pgpool.getconn(dbname), dbname, dt, app, schema, table, index)
 
 
-@routes.get(b'/%s/reindex/scheduled' % (T_DATABASE_NAME))
-def scheduled_reindex_database(http_context, app):
-    dbname = http_context['urlvars'][0]
+@bottle.get('/<dbname>/reindex/scheduled')
+def scheduled_reindex_database(dbname):
+    app = default_app().temboard
     return functions.list_scheduled_reindex(app, dbname=dbname)
 
 
-@routes.get(b'/%s/schema/%s/table/%s/reindex/scheduled' %
-            (T_DATABASE_NAME, T_SCHEMA_NAME, T_TABLE_NAME))
-def scheduled_reindex_table(http_context, app):
-    dbname = http_context['urlvars'][0]
-    schema = http_context['urlvars'][1]
+@bottle.get('/<dbname>/schema/<schema>/table/<table>/reindex/scheduled')
+def scheduled_reindex_table(dbname, schema, table):
+    app = default_app().temboard
+    return functions.list_scheduled_reindex(
+        app, dbname=dbname, schema=schema, table=table)
+
+
+@bottle.get('/<dbname>/schema/<schema>/reindex/scheduled')
+def scheduled_reindex_index(dbname, schema):
+    app = default_app().temboard
     return functions.list_scheduled_reindex(app, dbname=dbname, schema=schema)
 
 
-@routes.get(b'/%s/schema/%s/reindex/scheduled' % (T_DATABASE_NAME,
-                                                  T_SCHEMA_NAME))
-def scheduled_reindex_index(http_context, app):
-    dbname = http_context['urlvars'][0]
-    schema = http_context['urlvars'][1]
-    return functions.list_scheduled_reindex(app, dbname=dbname, schema=schema)
-
-
-@routes.delete(b'/reindex/' + T_OPERATION_ID)
-def delete_reindex(http_context, app):
-    reindex_id = http_context['urlvars'][0]
+@bottle.delete('/reindex/<reindex_id>')
+def delete_reindex(reindex_id):
+    app = default_app().temboard
     return functions.cancel_scheduled_operation(reindex_id, app)
 
 
-@routes.get(b'/reindex/scheduled')
+@bottle.get('/reindex/scheduled')
 def scheduled_reindex(http_context, app):
     return functions.list_scheduled_reindex(app)
 
@@ -296,9 +244,8 @@ class MaintenancePlugin:
         self.app = app
 
     def load(self):
-        self.app.router.add(routes)
+        default_app().mount('/maintenance', bottle)
         self.app.worker_pool.add(workers)
 
     def unload(self):
         self.app.worker_pool.remove(workers)
-        self.app.router.remove(routes)
