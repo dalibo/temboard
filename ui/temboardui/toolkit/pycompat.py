12894b03790b06a6b11ab6ba08742ad621163757
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-06-30 16:45:39 +0200

    ui: Download instances inventory as CSV

--- ui/temboardui/toolkit/pycompat.py
+++ ui/temboardui/toolkit/pycompat.py
@@ -11,9 +11,11 @@ if PY2:
     from urlparse import urlparse
     from urllib2 import HTTPError
     from urllib import quote_plus
+    from cStringIO import StringIO
 else:
     import configparser  # noqa: F401
     from collections import UserDict as IterableUserDict  # noqa: F401
     from queue import Empty  # noqa: F401
     from urllib.parse import quote_plus, urlparse  # noqa: F401
     from urllib.error import HTTPError  # noqa: F401
+    from io import StringIO  # noqa: F401
