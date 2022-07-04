49c576f4db8a06a0985655d7557307f050bf329a
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-07-04 22:54:20 +0200

    ui: Move download button aside filter

--- ui/temboardui/model/queries/copy-instances-as-csv.sql
+++ ui/temboardui/model/queries/copy-instances-as-csv.sql
@@ -1,27 +1,30 @@
 COPY (
-	SELECT DISTINCT
-		i.hostname AS "Hostname",
-		i.pg_port AS "Port",
-		i.pg_data AS "PGDATA",
-		i.pg_version_summary AS "Version",
-		string_agg(DISTINCT group_name, ',') AS "Groups",
-		i.agent_address AS "Agent Address",
-		i.agent_port AS "Agent Port",
-		string_agg(DISTINCT plugin_name, ',') AS "Plugins"
-	FROM application.instances AS i
-	LEFT OUTER JOIN application.instance_groups AS groups
-			 ON groups.agent_port = i.agent_port
-		  AND groups.agent_address = i.agent_address
-	LEFT OUTER JOIN application.plugins
-			 ON plugins.agent_address = i.agent_address
-		  AND plugins.agent_port = i.agent_port
-	GROUP BY 1, 2, 3, 4, 6, 7
-	HAVING concat_ws(
-	  ' ',
-		i.hostname, i.pg_port, i.pg_data, i.pg_version_summary,
-		i.agent_address, i.agent_port
+  WITH
+		inventory AS (SELECT DISTINCT
+			i.hostname AS "Hostname",
+			i.pg_port AS "Port",
+			i.pg_data AS "PGDATA",
+			i.pg_version_summary AS "Version",
+			string_agg(DISTINCT group_name, ',') AS "Groups",
+			i.agent_address AS "Agent Address",
+			i.agent_port AS "Agent Port",
+			string_agg(DISTINCT plugin_name, ',') AS "Plugins"
+		FROM application.instances AS i
+		LEFT OUTER JOIN application.instance_groups AS groups
+				ON groups.agent_port = i.agent_port
+				AND groups.agent_address = i.agent_address
+		LEFT OUTER JOIN application.plugins
+				ON plugins.agent_address = i.agent_address
+				AND plugins.agent_port = i.agent_port
+		GROUP BY 1, 2, 3, 4, 6, 7
+		ORDER BY 1, 2
+	)
+  SELECT * FROM inventory
+	WHERE concat_ws(
+		' ',
+		"Hostname", "Port", "PGDATA", "Version", "Groups",
+		"Agent Address", "Agent Port"
 	) LIKE %s
-	ORDER BY 1, 2
 ) TO STDOUT WITH (
 	DELIMITER ';',
 	ENCODING 'UTF-8',
