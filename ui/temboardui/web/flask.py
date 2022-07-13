915c0ce1fd84bddf044e8e4ab736e0640795742f
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-07-13 17:55:42 +0200

    ui: Add Flask database middleware

--- ui/temboardui/web/flask.py
+++ ui/temboardui/web/flask.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, g
+
+from ..model import Session
 
 
 def create_app(temboard_app):
     app = Flask('temboardui', static_folder=None)
     app.temboard = temboard_app
+    SQLAlchemy(app)
     return app
+
+
+class SQLAlchemy(object):
+    # Flask extension to manage a SQLAlchemy session per request.
+    def __init__(self, app=None):
+        self.app = app
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.db = self
+        app.before_request(self.before)
+        app.teardown_request(self.teardown)
+
+    def before(self):
+        g.db_session = Session()
+
+    def teardown(self, error):
+        if error:
+            # Expunge objects before rollback to implement
+            # expire_on_rollback=False. This allow templates to reuse
+            # request.instance object and joined object without triggering lazy
+            # load.
+            g.db_session.expunge_all()
+            g.db_session.rollback()
+        else:
+            g.db_session.commit()
+
+        g.db_session.close()
+        del g.db_session
