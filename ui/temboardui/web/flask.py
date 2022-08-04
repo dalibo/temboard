a470fe072af74ebf28363509dbd59519bd1dcadd
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-08-04 15:59:22 +0200

    ui: Extract Auth middleware

--- ui/temboardui/web/flask.py
+++ ui/temboardui/web/flask.py
@@ -17,6 +17,7 @@ def create_app(temboard_app):
     app.temboard = temboard_app
     SQLAlchemy(app)
     UserMiddleware(app)
+    AuthMiddleware(app)
     app.errorhandler(Exception)(json_error_handler)
     return app
 
@@ -56,9 +57,14 @@ class SQLAlchemy(object):
         del g.db_session
 
 
-class UserMiddleware(object):
-    # Flask extension to load current user and enforce login required and admin
-    # privileges.
+class AuthMiddleware(object):
+    # Flask extension enforcing authentication
+
+    def unauthorized(self):
+        if g.current_user:
+            abort(403)
+        return redirect('/login')
+
     def __init__(self, app=None):
         self.app = app
         if app:
@@ -68,27 +74,36 @@ class UserMiddleware(object):
         app.user = self
         app.before_request(self.before)
 
-    def unauthorized(self):
-        if g.current_user:
-            abort(403)
-        return redirect('/login')
-
     def before(self):
-        g.current_user = role = None
-        if 'temboard' in request.cookies:
-            g.current_user = role = self.load_user_from_tornado_secure_cookie()
-
         func = self.app.view_functions[request.endpoint]
 
         anonymous_allowed = getattr(func, '__anonymous_allowed', False)
-        if not anonymous_allowed and role is None:
+        if not anonymous_allowed and g.current_user is None:
             logger.debug("Redirecting anonymous to /login.")
             return redirect('/login')
 
         admin_required = getattr(func, '__admin_required', False)
-        if admin_required and not role.is_admin:
+        if admin_required and not g.current_user.is_admin:
             logger.debug("Refusing access to non-admin user.")
             abort(403)
+
+
+class UserMiddleware(object):
+    # Flask extension to load current user.
+
+    def __init__(self, app=None):
+        self.app = app
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.user = self
+        app.before_request(self.before)
+
+    def before(self):
+        g.current_user = None
+        if 'temboard' in request.cookies:
+            g.current_user = self.load_user_from_tornado_secure_cookie()
 
     def load_user_from_tornado_secure_cookie(self):
         cookie = decode_signed_value(
