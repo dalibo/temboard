af9e0ce587fa450023f07dc8018d8d5cb7d20bb3
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-08-23 18:59:39 +0200

    ui: Review about page

--- ui/temboardui/handlers/home.py
+++ ui/temboardui/handlers/home.py
@@ -1,6 +1,7 @@
 from ..application import (
     get_instance_groups_by_role,
 )
+from ..version import inspect_versions
 from ..web.tornado import (
     app,
     render_template,
@@ -18,4 +19,29 @@ def home(request):
         'home.html',
         nav=True, role=role,
         groups=groups,
+    )
+
+
+@app.route("/about")
+def metadata(request):
+    versions_info = inspect_versions()
+    infos = {
+        "Version": "%(temboard)s (%(temboardbin)s)" % versions_info,
+        "Uptime": app.start_time,
+        "OS": "%(distname)s %(distversion)s" % versions_info,
+        "Python": "%(python)s (%(pythonbin)s)" % versions_info,
+        "cryptography": "%(cryptography)s" % versions_info,
+        "Tornado": "%(tornado)s" % versions_info,
+        "libpq": "%(libpq)s" % versions_info,
+        "psycopg2": "%(psycopg2)s" % versions_info,
+        "SQLAlchemy": "%(sqlalchemy)s" % versions_info,
+    }
+    temboard_version = "%(temboard)s" % versions_info
+
+    return render_template(
+        'about.html',
+        nav=True,
+        role=request.current_user,
+        infos=infos,
+        temboard_version=temboard_version
     )
