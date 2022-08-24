9f4c442c843dcf16fe13b37843561ac0f7f5f601
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-08-24 13:59:00 +0200

    ui: Merge pretendu Start Bootstrap Admin 2 JS file

--- ui/temboardui/static/src/temboard.js
+++ ui/temboardui/static/src/temboard.js
@@ -4,3 +4,27 @@ import hljs from 'highlightjs'
 
 // Export hljs in global namespace.
 window.hljs = hljs;
+
+$(() => {
+  // Mark active link in sidebar
+  var url = window.location;
+  var element = $('ul.nav a').filter(function() {
+    return this.href == url || url.href.indexOf(this.href) == 0;
+  }).addClass('active').parent().parent().addClass('in').parent();
+
+  if (element.is('li')) {
+    element.addClass('active');
+  }
+
+  // Sidebar collapsology
+  if (eval(localStorage.getItem('sidebarCollapsed'))) {
+    document.body.className += 'sidebar-collapsed';
+  }
+
+  $('#menu-collapse').click(function(event) {
+    event.preventDefault();
+    $(document.body).toggleClass('sidebar-collapsed');
+    localStorage.setItem('sidebarCollapsed', $(document.body).hasClass('sidebar-collapsed'));
+    window.dispatchEvent(new Event('resize'));
+  });
+});
