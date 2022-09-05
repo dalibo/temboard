ac555385504df0f4090cca14d262bda62e55d4bc
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-09-05 18:34:43 +0200

    ui: Manage datatables with npm for Instances

--- ui/temboardui/static/src/temboard.settings.instance.js
+++ ui/temboardui/static/src/temboard.settings.instance.js
@@ -3,6 +3,17 @@ import DeleteInstanceDialog from './components/DeleteInstanceDialog.vue'
 import NewInstanceWizard from './components/NewInstanceWizard.vue'
 import UpdateInstanceDialog from './components/UpdateInstanceDialog.vue'
 
+import datatables from 'datatables.net-dt'
+import dtbuttons from 'datatables.net-buttons-dt'
+import dtbs4 from 'datatables.net-bs4'
+// Import only bootstrap4 styling. Importing other styles conflicts, producing
+// weird result.
+import 'datatables.net-bs4/css/dataTables.bootstrap4.css'
+
+datatables(window, $)
+dtbuttons(window, $)
+dtbs4(window, $)
+
 window.app = new Vue({
   el: "#vue-app",
   components: {
@@ -19,13 +30,13 @@ window.app = new Vue({
             id: "buttonDownload",
             "data-toggle": "tooltip"
           },
-          className: "btn-sm mx-1",
+          className: "btn btn-sm btn-secondary mx-1",
           text: `<i class="fa fa-download"></i>`,
-          action: function(e, dt, node, config) {
-            /*
-            * Use temBoard UI API instead of datatable export. UI export includes
-            * more data and has reordered column.
-            */
+          action: function() {
+            /**
+              * Use temBoard UI API instead of datatable export. UI export includes
+              * more data and has reordered column.
+              */
             var filter = $("#tableInstances_filter input").val();
             var url = new URLSearchParams({filter});
             window.location.replace("/settings/instances.csv?" + url.toString());
