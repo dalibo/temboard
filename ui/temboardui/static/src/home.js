601d6b694b67821576a66d43c3d6956f281947df
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-09-12 17:20:21 +0200

    ui: Move home Sparkline to SFC

--- ui/temboardui/static/src/home.js
+++ ui/temboardui/static/src/home.js
@@ -7,21 +7,13 @@ import Vue from 'vue/dist/vue.esm'
 import VueRouter from 'vue-router'
 
 import './fscreen.js'
+import Sparkline from './components/Sparkline.vue'
 
 
 Vue.use(VueRouter)
 
 
 var refreshInterval = 60 * 1000;
-
-Vue.component('sparkline', {
-  props: ['instance', 'metric'],
-  mounted: createChart,
-  watch: {
-    instance: createChart
-  },
-  template: '<div></div>'
-});
 
 Vue.component('checks', {
   props: ['instance'],
@@ -72,9 +64,12 @@ Vue.component('checks', {
 });
 
 
-var instancesVue = new Vue({
+window.instancesVue = new Vue({
   el: '#instances',
   router: new VueRouter(),
+  components: {
+    sparkline: Sparkline
+  },
   data: function() {
     var groupsFilter = [];
     if (this.$route.query.groups) {
@@ -177,104 +172,6 @@ function getStatusValue(instance) {
     value += checks.UNDEF;
   }
   return value;
-}
-
-function createChart() {
-  var api_url = ['/server', this.instance.agent_address, this.instance.agent_port, 'monitoring'].join('/');
-
-  var start = moment().subtract(1, 'hours');
-  var end = moment();
-  var defaultOptions = {
-    axes: {
-      x: {
-        drawAxis: false,
-        drawGrid: false
-      },
-      y: {
-        drawAxis: false,
-        drawGrid: false
-      }
-    },
-    dateWindow: [start, end],
-    legend: 'never',
-    xValueParser: function(x) {
-      var m = moment(x);
-      return m.toDate().getTime();
-    },
-    highlightCircleSize: 0,
-    interactionModel: {}
-  };
-
-  var options = defaultOptions;
-  switch (this.metric) {
-  case 'load1':
-    options = $.extend({colors: ['#FAA43A']}, options);
-    break;
-  case 'tps':
-    options = $.extend({colors: ['#50BD68', '#F15854']}, options);
-    break;
-  }
-
-  var params = "?start=" + start.toISOString() + "&end=" + end.toISOString();
-  var metricsUrl = api_url + "/data/" + this.metric + params;
-  var data = null;
-  var dataReq = $.get(metricsUrl, function(_data) {
-    data = _data;
-  });
-  // Get the dates when the instance was unavailable
-  var unavailabilityData = '';
-  var promise = $.when(dataReq);
-  var unavailabilityUrl = api_url + '/unavailability' + params;
-  if (this.metric == 'tps') {
-
-    promise = $.when(dataReq,
-      $.get(unavailabilityUrl, function(_data) { unavailabilityData = _data; })
-    );
-  }
-  promise.then(function() {
-    // fill unavailability data with NaN
-    var colsCount = data.split('\n')[0].split(',').length;
-    var nanArray = new Array(colsCount - 1).fill('NaN');
-    nanArray.unshift('');
-    unavailabilityData = unavailabilityData.replace(/\n/g, nanArray.join(',') + '\n');
-
-    var chart = new Dygraph(
-      this.$el,
-      data + unavailabilityData,
-      options
-    );
-    var last;
-    if (this.metric == 'tps') {
-      var lastCommit = chart.getValue(chart.numRows() - 1, 1);
-      var lastRollback = chart.getValue(chart.numRows() - 1, 2);
-      last = lastCommit + lastRollback;
-    } else {
-      last = chart.getValue(chart.numRows() - 1, 1);
-    }
-    this.instance['current' + _.capitalize(this.metric)] = last;
-    instancesVue.$forceUpdate();
-  }.bind(this));
-}
-
-function abbreviateNumber(number) {
-  var abbrev = [ "k", "m", "b", "t" ];
-
-  for (var i = abbrev.length - 1; i >= 0; i--) {
-
-    var size = Math.pow(10, (i + 1) * 3);
-
-    if(size <= number) {
-      number = Math.round(number * 10 / size) / 10;
-      if((number == 1000) && (i < abbrev.length - 1)) {
-        number = 1;
-        i++;
-      }
-      number += abbrev[i];
-      break;
-    }
-  }
-
-  return number;
 }
 
 function fetchInstances() {
