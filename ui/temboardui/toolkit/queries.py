401c9f22554d4a5d9804f5cec20e8c51aea85658
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-09-13 16:44:09 +0200

    Comment query origin

--- ui/temboardui/toolkit/queries.py
+++ ui/temboardui/toolkit/queries.py
@@ -1,4 +1,5 @@
 import os.path
+from textwrap import dedent
 
 
 class QueryFiler(dict):
@@ -9,11 +10,17 @@ class QueryFiler(dict):
         super(QueryFiler, self).__init__()
 
     def load(self):
+        fmt = dedent("""\
+        -- From %s
+        %s
+        """)
+
         for path in self.iter_files():
             filename = os.path.basename(path)
             name, ext = os.path.splitext(filename)
             with open(path) as fo:
-                self[name] = fo.read()
+                sql = fo.read()
+            self[name] = fmt % (path, sql)
 
     def iter_files(self):
         for filename in os.listdir(self.path):
