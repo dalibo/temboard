53d16149e733be0fbf8456b5c0642946082909c0
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-09-22 10:47:01 +0200

    Version 8.0b1.post6

--- agent/temboardagent/version.py
+++ agent/temboardagent/version.py
@@ -1,1 +1,1 @@
-__version__ = "8.0b1.post5"
+__version__ = "8.0b1.post6"
--- ui/temboardui/version.py
+++ ui/temboardui/version.py
@@ -2,7 +2,7 @@ import sys
 from platform import python_version
 
 
-__version__ = "8.0b1.post5"
+__version__ = "8.0b1.post6"
 
 
 # This output is parsed by tests/conftest.py::pytest_report_header.
