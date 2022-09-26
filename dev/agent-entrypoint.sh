433e050939157f7eeccf9614380d020db0cdeeb4
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-09-26 17:05:59 +0200

    dév: Fix UI key prefetch

--- dev/agent-entrypoint.sh
+++ dev/agent-entrypoint.sh
@@ -9,10 +9,12 @@ pip install --ignore-installed --no-deps -e /usr/local/src/temboard/agent/
 temboard-agent --version
 chown -R "$(stat -c "%u:%g" "$0")" /usr/local/src/temboard/agent
 
-if ! [ -f /etc/temboard-agent/signing-public.pem ] ; then
-	# Copy dev signing key to prefetch UI key.
-	mkdir -p /etc/temboard-agent/
-	cp -v /usr/local/src/temboard/dev/signing-public.pem /etc/temboard-agent/
+if ! [ -f /etc/temboard-agent/postgres0/signing-public.pem ] ; then
+	# Copy dev signing key to prefetch UI key. Don't bother whether we are
+	# managing first or second instance.
+	mkdir -p /etc/temboard-agent/postgres{0,1}
+	cp -fv /usr/local/src/temboard/dev/signing-public.pem /etc/temboard-agent/postgres0
+	cp -fv /usr/local/src/temboard/dev/signing-public.pem /etc/temboard-agent/postgres1
 fi
 
 exec /usr/local/src/temboard/agent/packaging/docker/entrypoint.sh "$@"
