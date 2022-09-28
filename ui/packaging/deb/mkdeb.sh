40bc67af30f48e3eb0b3f82d181edbbe2fd76f78
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-09-28 09:21:51 +0200

    ui: Use Psycopg2 2.9
    There is no binary for Psycopg2 2.8 - Python 3.10.

--- ui/packaging/deb/mkdeb.sh
+++ ui/packaging/deb/mkdeb.sh
@@ -56,7 +56,7 @@ dist="$DISTDIR/temboard-$pep440v"-py2.py3-none-any.whl
 if ! [ -f "$dist" ] ; then
 	pip download --only-binary :all: --no-deps --pre --dest "$DISTDIR/" "temboard==$pep440v"
 fi
-pip install --only-binary cffi,cryptography "$dist" 'psycopg2-binary<2.9'
+pip install --only-binary cffi,cryptography,psycopg2-binary "$dist" psycopg2-binary
 pip check
 virtualenv --python="$PYTHON" --relocatable "$VIRTUAL_ENV"
 
