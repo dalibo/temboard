dfc69ccc7eac12a2eb271b68fba716e726222411
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-10-14 10:44:30 +0200

    ui: Drop dead code

--- ui/temboardui/static/src/public/js/temboard.settings.user.js
+++ ui/temboardui/static/src/public/js/temboard.settings.user.js
@@ -356,45 +356,6 @@ function send_add_user_form(modal_id)
   });
 }
 
-function sendEmail() {
-  $.ajax({
-    url: '/json/test_email',
-    type: 'post',
-    data: JSON.stringify({
-      email: $('#inputTestEmail').val()
-    }),
-    success: function(data) {
-      var msg = 'Test email successfully sent';
-      msg += '\nIf the email is not received, please have a look at your SMTP server logs';
-      alert(msg);
-    },
-    error: function(xhr) {
-      var msg = 'Mail could not be sent.';
-      if (xhr.responseText) {
-        msg += '\nError: ';
-        msg += escapeHtml(JSON.parse(xhr.responseText).error);
-      }
-      alert(msg);
-    }
-  });
-}
-
-function sendSms() {
-  $.ajax({
-    url: '/json/test_sms',
-    type: 'post',
-    data: JSON.stringify({
-      phone: $('#inputTestPhone').val()
-    }),
-    success: function(data) {
-      alert("Test SMS sent");
-    },
-    error: function(xhr) {
-      alert("SMS could not be sent.\nError: " + escapeHtml(JSON.parse(xhr.responseText).error));
-    }
-  });
-}
-
 var entityMap = {
   "&": "&amp;",
   "<": "&lt;",
