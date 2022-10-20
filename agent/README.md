c50e0004f0a4121efd0c680962d453cc2513217f
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-10-20 15:19:32 +0200

    agent: Move README to markdown

---
+++ agent/README.md
@@ -0,0 +1,5 @@
+# temBoard Agent
+
+This project contains the service monitoring and managing one PostgreSQL instance.
+
+Please refer to [temBoard](https://labs.dalibo.com/temboard) main project to see the full picture.
--- agent/setup.py
+++ agent/setup.py
@@ -61,6 +61,6 @@ setup(**dict(
         ],
     },
     packages=find_packages(),
-    long_description=open('README.rst').read(),
-    long_description_content_type='text/x-rst',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 ))
