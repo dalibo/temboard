e8449b2abe802ce50cefdacd33780e42991c6f46
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-10-24 09:49:33 +0200

    ui: Fix alembic migration script

--- ui/temboardui/model/versions/000_init.sql
+++ ui/temboardui/model/versions/000_init.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS application;
 CREATE TABLE IF NOT EXISTS application.schema_migration_log (
   version TEXT UNIQUE NOT NULL PRIMARY KEY,
-  cdate TIMESTAMP DEFAULT NOW(),
-	comment TEXT
+	comment TEXT,
+  cdate TIMESTAMP DEFAULT NOW()
 );
