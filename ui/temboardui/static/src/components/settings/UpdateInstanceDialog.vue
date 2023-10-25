ff7256761696078e419d9856a427375c47a875d6
Étienne BERSAC <etienne.bersac@dalibo.com>
2023-10-25 14:39:58 +0200

    ui: Drop key when updating new agent

--- ui/temboardui/static/src/components/settings/UpdateInstanceDialog.vue
+++ ui/temboardui/static/src/components/settings/UpdateInstanceDialog.vue
@@ -154,6 +154,11 @@ export default {
     },
     update(data) {
       this.waiting = true;
+      if (this.discover_data.signature_status === "valid") {
+        // Ensure agent key is dropped for new agents.
+        data.agent_key = null;
+      }
+
       $.ajax({
         url: ["/json/settings/instance", this.agent_address, this.agent_port].join("/"),
         method: "POST",
