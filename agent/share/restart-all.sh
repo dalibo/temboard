8b4d75ffe3d08590c4f908da41e6051bbe99300e
Étienne BERSAC <etienne.bersac@dalibo.com>
2023-11-09 09:15:08 +0100

    agent: Golf useless search of systemctl binary

--- agent/share/restart-all.sh
+++ agent/share/restart-all.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -eu
 
-if ! hash systemctl &>/dev/null; then
-	echo You must restart manually temboard-agent services. >&2
-	exit 0
-fi
-
 if ! readlink /proc/1/exe | grep -q systemd ; then
 	echo You must restart manually temboard-agent services. >&2
 	exit 0
