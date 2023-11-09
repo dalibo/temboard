c9f48bd3daadc2bd301be0fc53bd58914404c503
Étienne BERSAC <etienne.bersac@dalibo.com>
2023-11-09 09:14:10 +0100

    ui: Reload and restart only running

--- ui/packaging/deb/postinst.sh
+++ ui/packaging/deb/postinst.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -eu
 
-if systemctl is-active temboard &>/dev/null ; then
-	systemctl restart temboard
+if ! readlink /proc/1/exe | grep -q systemd ; then
+	echo You must restart manually temboard service. >&2
+	exit 0
 fi
+
+systemctl daemon-reload
+
+# Restart if running.
+systemctl try-restart temboard
