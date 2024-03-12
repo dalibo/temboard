6947f14d5e4df13ac52e94707be8ce6e683114d9
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-03-12 08:50:27 +0100

    Remove obsolete relative-time vue filter
    It's now handled with VueUse UseTimeAgo.

--- ui/temboardui/plugins/maintenance/static/js/vue.filter.relative-time.js
+++
@@ -1,8 +0,0 @@
-/* global Vue, moment */
-Vue.filter("relative_time", function (value, allowNegative) {
-  allowNegative = allowNegative === false ? false : true;
-  if (!value || (!allowNegative && moment(value).diff(moment()) < 0)) {
-    return "now";
-  }
-  return moment(value).fromNow();
-});
