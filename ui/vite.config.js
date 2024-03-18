6d63c901a39f1126ccad3fed2fb7b191ff634d3c
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-03-18 15:21:07 +0100

    Preserve white space when compiling Vue templates
    May be removed when switching to Vue3.

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,7 +3,15 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          whitespace: "preserve",
+        },
+      },
+    }),
+  ],
   root: "temboardui/static/src",
   base: "/static/",
   server: {
