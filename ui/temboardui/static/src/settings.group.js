3ec61437b9dcaf9926b18b8692c9e302a52a2961
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-04-09 14:27:57 +0200

    Upgrade settings.group pages to Vue3

--- ui/temboardui/static/src/settings.group.js
+++ ui/temboardui/static/src/settings.group.js
@@ -1,22 +1,11 @@
-// This script requires a global variable group_king.
-import dtbs4 from "datatables.net-bs4";
-// Import only bootstrap4 styling. Importing other styles conflicts, producing
-// weird result.
-import "datatables.net-bs4/css/dataTables.bootstrap4.css";
-import dtbuttons from "datatables.net-buttons-dt";
-import datatables from "datatables.net-dt";
-import Vue from "vue";
+import DataTable from "datatables.net-bs4";
+import { createApp } from "vue";
 
 import DeleteGroupDialog from "./components/settings/DeleteGroupDialog.vue";
 import EnvironmentMigrationDialog from "./components/settings/EnvironmentMigrationDialog.vue";
 import UpdateGroupDialog from "./components/settings/UpdateGroupDialog.vue";
 
-datatables(window, $);
-dtbuttons(window, $);
-dtbs4(window, $);
-
-window.app = new Vue({
-  el: "#vue-app",
+createApp({
   components: {
     "environment-migration-dialog": EnvironmentMigrationDialog,
     "delete-group-dialog": DeleteGroupDialog,
@@ -24,7 +13,7 @@ window.app = new Vue({
   },
   created() {
     this.$nextTick(() => {
-      var table = $("#tableGroups").DataTable({ stateSave: true });
+      new DataTable("#tableGroups", { stateSave: true });
     });
   },
-});
+}).mount("#vue-app");
