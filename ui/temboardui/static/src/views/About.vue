53796436a3bcaac82fe7af8cc5c5e8611ddeccc1
Étienne BERSAC <etienne.bersac@dalibo.com>
2024-06-11 09:24:06 +0200

    about: Add usual project links

--- ui/temboardui/static/src/views/About.vue
+++ ui/temboardui/static/src/views/About.vue
@@ -18,6 +18,20 @@ function getMetadata() {
         <div class="row">
           <div class="col-12 text-center">
             <h1 style="font-size: 3rem; color: #d65a16" class="font-weight-bold">temBoard {{ temboard_version }}</h1>
+            <div class="btn-group mx-auto justify-content-center">
+              <a href="https://labs.dalibo.com/temboard" class="badge badge-light mx-1" target="_blank">
+                <i class="fa fa-globe"></i> Official website
+              </a>
+              <a href="https://temboard.readthedocs.io/en/latest/" class="badge badge-light mx-1" target="_blank">
+                <i class="fa fa-book"></i> Documentation
+              </a>
+              <a href="https://github.com/dalibo/temboard" class="badge badge-light mx-1" target="_blank">
+                <i class="fa fa-github"></i> GitHub
+              </a>
+              <a href="https://github.com/dalibo/temboard/issues/new" class="badge badge-light mx-1" target="_blank">
+                <i class="fa fa-bug"></i> New issue
+              </a>
+            </div>
           </div>
         </div>
         <div style="position: relative">
