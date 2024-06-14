8b20b2d7b64be532892a23d62b28a545ec5f4437
Pierre-Louis GONON <pierre-louis.gonon@dalibo.com>
2024-06-14 11:26:51 +0200

    Complete instance settings tests
    We add a test for selected groups in
    test_20_inventory and a test for deleting an instance
    at the end of the tests.

---
+++ tests/test_99_delete_instance.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture(scope="module")
+def browse_settings_instance(browse_instance, browser_session):
+    """Go to Settings tab of current instance."""
+    browser_session.select("#linkSettings").click()
+    browser_session.select(
+        "ul.list-group a[href='/settings/instances']"
+    ).click()
+
+
+def test_delete_instance(browser, browse_settings_instance, agent_conf):
+    browser.select("td button.buttonDelete").click()
+    browser.select("#modalDeleteInstance button#buttonDelete").click()
+    port = agent_conf.get("temboard", "port")
+    # Check the instance has been deleted
+    assert all(
+        f"0.0.0.0:{port}" not in tr.text
+        for tr in browser.select_all("#tableInstances tbody tr")
+    )
