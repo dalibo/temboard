81101e44305d8d7ded9e9983f89d440881ac9361
Étienne BERSAC <etienne.bersac@dalibo.com>
2024-06-28 09:45:49 +0200

    services: Handle external command as service

--- ui/temboardui/toolkit/services.py
+++ ui/temboardui/toolkit/services.py
@@ -33,6 +33,7 @@
 
 import logging
 import os
+import pathlib
 import signal
 import time
 
@@ -42,7 +43,7 @@ logger = logging.getLogger(__name__)
 
 
 def run(main, *backgrounds):
-    """Execute a main service object function in this process.
+    """Run a main service object function in this process.
 
     Handle signals for INT, TERM, HUP, CHLD and ALRM.
     Handle background services.
@@ -61,8 +62,7 @@ def run(main, *backgrounds):
     sgm.register(LoopStopper(loop))
     sgm.register(main)
 
-    # for tornado autoreload. See TornadoService._autoreload_hook.
-    main.background = bg = BackgroundManager(loop)
+    bg = BackgroundManager(loop)
     if backgrounds:
         for service in backgrounds:
             bg.add(service)
@@ -76,6 +76,25 @@ def run(main, *backgrounds):
         main.teardown()
     logger.debug("Done. service=%s", main)
     return 0
+
+
+def execute(service):
+    """Execute an external command in this process.
+
+    Replace current Python program by a command.
+    """
+
+    if hasattr(service, "setup"):
+        service.setup()
+
+    # forking from asyncio loop requires reset of wakeup_fd.
+    signal.set_wakeup_fd(-1)
+    for fd in range(3, os.sysconf("SC_OPEN_MAX")):
+        try:
+            os.close(fd)
+        except OSError:
+            pass
+    os.execvp(service.command[0], service.command)
 
 
 class LoopStopper:
@@ -107,9 +126,20 @@ class BackgroundManager:
     def add(self, service):
         self.services[str(service)] = service
 
+    def __bool__(self):
+        return bool(self.services)
+
     def __enter__(self):
         if not self.services:
             return
+
+        self._read_pids()
+
+        if self.pids:
+            logger.debug("Cleaning previous background services.")
+            self.kill()
+            if self.wait():
+                raise Exception("Background services are still alive.")
         self.start()
 
     def __exit__(self, *a):
@@ -129,17 +159,49 @@ class BackgroundManager:
         pid = os.fork()
         if pid:  # Parent process
             logger.debug("Background service started. service=%s pid=%d", service, pid)
-            self.pids[str(service)] = pid
+            self._save_pid(service, pid)
             return pid
 
         # Child process
-        os._exit(run(service))
+        if hasattr(service, "pidfile"):
+            execute(service)
+        else:
+            os._exit(run(service))
+
+    def _read_pids(self):
+        for name, service in self.services.items():
+            if not hasattr(service, "pidfile"):
+                continue
+            if not os.path.exists(service.pidfile):
+                continue
+            with open(service.pidfile) as fo:
+                self.pids[name] = int(fo.read().strip())
+                logger.debug(
+                    "Read pid from pidfile. service=%s pid=%d", name, self.pids[name]
+                )
+
+    def _save_pid(self, service, pid):
+        self.pids[str(service)] = pid
+        if hasattr(service, "pidfile"):
+            with open(service.pidfile, "w") as fo:
+                fo.write(str(pid))
+
+    def _drop_pid(self, name):
+        del self.pids[name]
+        s = self.services[name]
+        if not hasattr(s, "pidfile"):
+            return
+        if os.path.exists(s.pidfile):
+            os.unlink(s.pidfile)
 
     def stop(self):
         self.stopping = True
         for name, pid in self.pids.items():
             logger.debug("Terminating background service. service=%s pid=%d", name, pid)
-            os.kill(pid, signal.SIGTERM)
+            try:
+                os.kill(pid, signal.SIGTERM)
+            except ProcessLookupError:
+                pass
         time.sleep(0.125)
         if self.wait():
             self.kill()
@@ -157,20 +219,25 @@ class BackgroundManager:
                 try:
                     pid, status = os.waitpid(-1, os.WNOHANG)
                 except ChildProcessError:
-                    break
+                    pass
                 if pid:
-                    del self.pids[name]
+                    self._drop_pid(name)
 
             time.sleep(step)
             timeout -= step
 
         return bool(self.pids)
 
-    def kill(self):
+    def kill(self, sig=signal.SIGKILL):
         for name, pid in self.pids.items():
-            logger.warning("Killing background service. service=%s pid=%s", name, pid)
+            logger.warning(
+                "Signaling background service. service=%s pid=%s signal=%s",
+                name,
+                pid,
+                sig,
+            )
             try:
-                os.kill(pid, signal.SIGKILL)
+                os.kill(pid, sig)
             except ProcessLookupError:
                 pass
 
@@ -192,11 +259,9 @@ class BackgroundManager:
                 pass
 
             logger.warning(
-                "Background service dead. Restarting service=%s pid=%s",
-                name,
-                self.pids[name],
+                "Background service dead. Restarting service=%s pid=%s", name, pid
             )
-            del self.pids[name]
+            self._drop_pid(name)
 
         self.start()
 
