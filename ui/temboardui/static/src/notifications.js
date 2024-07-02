536e0cfc873d828b71b386cc112d74a5afebf0e6
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-07-02 12:32:50 +0200

    Upgrade Datatables to datatables.net-vue3
    We also use the bootstrap 5 styling even though bootstrap 5 is not yet used in the
    project. It seems to be harmless for the moment. The overall appearance look good.

--- ui/temboardui/static/src/notifications.js
+++ ui/temboardui/static/src/notifications.js
@@ -1,10 +1,13 @@
-import DataTable from "datatables.net-bs4";
+import DataTablesLib from "datatables.net-bs5";
+import DataTable from "datatables.net-vue3";
 import { createApp } from "vue";
+
+DataTable.use(DataTablesLib);
 
 createApp({
   created() {
     this.$nextTick(() => {
-      new DataTable("#tableNotifications", {
+      new DataTablesLib("#tableNotifications", {
         order: [[0, "desc"]],
         pageLength: 25,
       });
