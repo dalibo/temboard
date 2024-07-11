39dc5cffd74b79c2fe17eb683a97bed958ecb03d
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-07-11 08:23:14 +0200

    Remove unrequired Vue dependency for notifications page

--- ui/temboardui/static/src/notifications.js
+++ ui/temboardui/static/src/notifications.js
@@ -1,16 +1,9 @@
 import DataTablesLib from "datatables.net-bs5";
 import DataTable from "datatables.net-vue3";
-import { createApp } from "vue";
 
 DataTable.use(DataTablesLib);
 
-createApp({
-  created() {
-    this.$nextTick(() => {
-      new DataTablesLib("#tableNotifications", {
-        order: [[0, "desc"]],
-        pageLength: 25,
-      });
-    });
-  },
-}).mount("#app");
+new DataTablesLib("#tableNotifications", {
+  order: [[0, "desc"]],
+  pageLength: 25,
+});
