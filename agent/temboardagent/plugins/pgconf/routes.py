4a08b275f64149f03fdd38a680c1b6d117ae4ee7
Étienne BERSAC <etienne.bersac@dalibo.com>
2024-07-17 11:43:17 +0200

    pgconf: Don't call post_settings for empty
    This case appears when using POST /configuration for reseting a single parameter.

--- agent/temboardagent/plugins/pgconf/routes.py
+++ agent/temboardagent/plugins/pgconf/routes.py
@@ -26,7 +26,7 @@ def get_settings(pgconn):
 @bottle.post("/settings")
 def post_settings(pgconn, new=None):
     """Applies a JSON mapping of setting -> value."""
-    new = new or request.json
+    new = request.json if new is None else new
     if not hasattr(new, "items"):
         raise HTTPError(406, "Requires a mapping of settings and values.")
     if not new:
@@ -105,7 +105,9 @@ def post_configuration(pgconn):
         for i in request.json["settings"]
         if i["name"] not in reset
     }
-    out = post_settings(pgconn, new)
+    if new:
+        out = post_settings(pgconn, new)
+
     if out is None:
         out = {"settings": []}
     return out
