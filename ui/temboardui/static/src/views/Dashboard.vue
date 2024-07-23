dalibo/temboard

commit 999a9bf3d39650e6beaa7533e07cf1c6d88c1c6d

Load jQuery and bootstrap from npm packages
We don't load jquery with <script> tag in the base templates.

jQuery built library is still used in the configuration and settings.notifications pages.
So we specifically load it using a <script> tag for those.

Changes to the $.ajax API have been made since we upgraded from 2.x to 3.x.

ui/temboardui/static/src/views/Dashboard.vue
@@ -2,6 +2,7 @@
 import { UseTimeAgo } from "@vueuse/components";
 import { useFullscreen } from "@vueuse/core";
 import { filesize } from "filesize";
+import $ from "jquery";
 import { computed, onMounted, ref } from "vue";
 
 // FIXME import chart.js and moment
@@ -212,16 +213,19 @@ function updateTps(data) {
   const commitData = datasets[0].data;
   const rollbackData = datasets[1].data;
 
-  if (data.length > 1) {
-    // Initial call. Bootstrap first datum.
-    lastDatum = data[0];
-    data.shift();
+  if (data.length <= 1) {
+    return;
   }
+
+  // Initial call. Bootstrap first datum.
+  lastDatum = data[0];
+  data.shift();
 
   let duration;
   for (let i = 0; i < data.length; i++) {
     const datum = data[i];
     const databases = datum.databases;
+
     duration = datum.timestamp - lastDatum.timestamp;
     if (duration === 0) {
       continue;
@@ -279,7 +283,7 @@ function updateAlerts() {
   $.ajax({
     url: "/server/" + props.instance.agentAddress + "/" + props.instance.agentPort + "/alerting/alerts.json",
   })
-    .success(function (data) {
+    .done(function (data) {
       // remove any previous popover to avoid conflicts with
       // recycled div elements
       $(divAlertsEl.value).find("[data-toggle-popover]").popover("dispose");
@@ -298,7 +302,7 @@ function updateAlerts() {
           });
       }, 1);
     })
-    .error(function (error) {
+    .fail(function (error) {
       // FIXME handle error
       console.error(error);
     });
@@ -306,10 +310,10 @@ function updateAlerts() {
   $.ajax({
     url: "/server/" + props.instance.agentAddress + "/" + props.instance.agentPort + "/alerting/checks.json",
   })
-    .success(function (data) {
+    .done(function (data) {
       states.value = data;
     })
-    .error(function (error) {
+    .fail(function (error) {
       // FIXME handle error
       console.error(error);
     });

ui/temboardui/plugins/pgconf/templates/configuration.html
@@ -6,6 +6,7 @@
 <!-- Toggle -->
 <link href="/css/bootstrap-toggle.min.css" rel="stylesheet">
 <link href="/css/pgconf/temboard.pgconf.css" rel="stylesheet">
+<script src="/js/jquery-2.1.4.min.js"></script>
 {% end %}
 
 {% block content %}

ui/temboardui/static/src/components/AlertingChart.vue
@@ -1,4 +1,5 @@
 <script setup>
+import $ from "jquery";
 import moment from "moment";
 import { inject, onMounted, ref, watch } from "vue";
 

ui/temboardui/static/src/components/DateRangePicker/DateRangePicker.vue
@@ -1,5 +1,6 @@
 <script setup>
 import daterangepicker from "daterangepicker";
+import $ from "jquery";
 import * as _ from "lodash";
 import moment from "moment";
 import { computed, ref, watch } from "vue";

ui/temboardui/static/src/components/ModalDialog.vue
@@ -1,5 +1,6 @@
 <script setup>
 /* A simple boostrap Dialog */
+import $ from "jquery";
 import { onMounted, ref } from "vue";
 
 defineProps(["id", "title"]);

ui/temboardui/static/src/components/MonitoringChart.vue
@@ -1,4 +1,5 @@
 <script setup>
+import $ from "jquery";
 import moment from "moment";
 import { onMounted, ref, watch } from "vue";
 

ui/temboardui/static/src/components/home/InstanceCard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Dygraph from "dygraphs";
 import "dygraphs/dist/dygraph.css";
+import $ from "jquery";
 import _ from "lodash";
 import { computed, onMounted, ref, watch } from "vue";
 

ui/temboardui/static/src/components/maintenance/AnalyzeModal.vue
@@ -1,5 +1,6 @@
 <script setup>
 import daterangepicker from "daterangepicker";
+import $ from "jquery";
 import moment from "moment";
 import { onMounted, ref } from "vue";
 

commit 74f5dbd2d86210d76817760fc56576ef7bc596a8

ui: Fix TPS chart not updating after first load in dashboard
Bug introduced in 999a9bf

ui/temboardui/static/src/views/Dashboard.vue
@@ -208,13 +208,11 @@ function updateTps(data) {
   const commitData = datasets[0].data;
   const rollbackData = datasets[1].data;
 
-  if (data.length <= 1) {
-    return;
+  if (data.length > 1) {
+    // Initial call. Bootstrap first datum.
+    lastDatum = data[0];
+    data.shift();
   }
-
-  // Initial call. Bootstrap first datum.
-  lastDatum = data[0];
-  data.shift();
 
   let duration;
   for (let i = 0; i < data.length; i++) {