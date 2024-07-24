3d2834358eb98bcf2c32359f734fd6be51f0cac1
Étienne BERSAC <etienne.bersac@dalibo.com>
2024-07-24 15:07:20 +0200

    ui: Refuse bad typed value in column
    Let Postgres or SQLAlchemy fail.

--- ui/temboardui/model/orm.py
+++ ui/temboardui/model/orm.py
@@ -1,14 +1,12 @@
-import datetime
 import string
 from secrets import choice
 
-from past.builtins import basestring
-from sqlalchemy import Boolean, DateTime, Integer, String, event, text
+from sqlalchemy import text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm.query import Query
 from sqlalchemy.schema import Column
-from sqlalchemy.types import TIMESTAMP, BigInteger, UnicodeText
+from sqlalchemy.types import TIMESTAMP, BigInteger, Integer, UnicodeText
 
 from temboardui.model import tables
 
@@ -16,77 +14,6 @@ from ..toolkit.utils import utcnow
 from . import QUERIES
 
 Model = declarative_base()
-
-
-def validate_int(value):
-    if value is None:
-        return
-    if isinstance(value, str):
-        value = int(value)
-    if isinstance(value, basestring):
-        value = int(value)
-    assert isinstance(value, int)
-    return value
-
-
-def validate_string(value):
-    assert isinstance(value, basestring)
-    return value
-
-
-def validate_datetime(value):
-    assert isinstance(value, datetime.datetime)
-    return value
-
-
-def validate_boolean(value):
-    if isinstance(value, int):
-        if value == 1:
-            value = True
-        if value == 0:
-            value = False
-    if isinstance(value, basestring):
-        if (
-            str(value).lower() == "yes"
-            or str(value).lower() == "true"
-            or str(value).lower() == "t"
-            or value == "1"
-        ):
-            value = True
-        if (
-            str(value).lower() == "no"
-            or str(value).lower() == "false"
-            or str(value).lower() == "f"
-            or value == "0"
-        ):
-            value = False
-    assert isinstance(value, bool)
-    return value
-
-
-validators = {
-    Integer: validate_int,
-    String: validate_string,
-    DateTime: validate_datetime,
-    Boolean: validate_boolean,
-}
-
-
-@event.listens_for(Model, "attribute_instrument")
-def configure_listener(class_, key, inst):
-    if not hasattr(inst.property, "columns"):
-        return
-
-    @event.listens_for(inst, "set", retval=True)
-    def set_(instance, value, oldvalue, initiator):
-        validator = validators.get(inst.property.columns[0].type.__class__)
-        if validator:
-            try:
-                return validator(value)
-            except AssertionError:
-                raise Exception("%s: wrong type." % (inst.property))
-        else:
-            return value
 
 
 class ApiKeys(Model):
