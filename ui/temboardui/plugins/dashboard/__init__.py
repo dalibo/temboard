6da9ac73e56c5661cfe5c18d44f14b9d1a1e4f5c
Pierre-Louis GONON <pierre-louis.gonon@dalibo.com>
2024-07-30 14:35:30 +0200

    Migrate dashboard plugin to Flask

--- ui/temboardui/plugins/dashboard/__init__.py
+++ ui/temboardui/plugins/dashboard/__init__.py
@@ -1,17 +1,4 @@
-import logging
-from os.path import realpath
-
-import tornado.web
-from tornado.escape import json_encode
-
-from ...agentclient import TemboardAgentClient
-from ...web.tornado import Blueprint, TemplateRenderer
-
-blueprint = Blueprint()
-blueprint.generic_proxy(r"/dashboard")
-logger = logging.getLogger(__name__)
-plugin_path = realpath(__file__ + "/..")
-render_template = TemplateRenderer(plugin_path + "/templates")
+from ...web.flask import instance_proxy
 
 
 class DashboardPlugin:
@@ -19,40 +6,5 @@ class DashboardPlugin:
         self.app = app
 
     def load(self):
-        self.app.tornado_app.add_rules(blueprint.rules)
-        self.app.tornado_app.add_rules(
-            [
-                (
-                    r"/js/dashboard/(.*)",
-                    tornado.web.StaticFileHandler,
-                    {"path": plugin_path + "/static/js"},
-                )
-            ]
-        )
-
-
-@blueprint.instance_route(r"/dashboard")
-def dashboard(request):
-    request.instance.check_active_plugin("dashboard")
-
-    try:
-        config = request.instance.get("/dashboard/config")
-    except TemboardAgentClient.Error as e:
-        if 404 != e.code:
-            raise
-        logger.debug("Fallback dashboard config.")
-        config = {"history_length": 150, "scheduler_interval": 2}
-
-    history = request.instance.get("/dashboard/history")
-
-    request.instance.fetch_status()
-    return render_template(
-        "dashboard.html",
-        nav=True,
-        role=request.current_user,
-        instance=request.instance,
-        plugin="dashboard",
-        config=json_encode(config),
-        dashboard=history[-1] if history else {},
-        history=json_encode(history or ""),
-    )
+        __import__(__name__ + ".routes")
+        instance_proxy.generic_proxy("/dashboard")
---
+++ ui/temboardui/plugins/dashboard/routes.py
@@ -0,0 +1,37 @@
+import logging
+
+from flask import current_app, g, render_template
+
+from ...agentclient import TemboardAgentClient
+from ...web.flask import instance_routes
+
+logger = logging.getLogger(__name__)
+
+
+@instance_routes.route("/dashboard")
+def dashboard():
+    current_app.instance.check_active_plugin("dashboard")
+
+    try:
+        config = current_app.instance.request("/dashboard/config").json()
+    except TemboardAgentClient.Error as e:
+        if 404 != e.code:
+            raise
+        logger.debug("Fallback dashboard config.")
+        config = {"history_length": 150, "scheduler_interval": 2}
+
+    history = current_app.instance.request("/dashboard/history").json()
+
+    current_app.instance.fetch_status()
+    dashboard = history[-1] if history else {}
+    return render_template(
+        "dashboard.html",
+        nav=True,
+        role=g.current_user,
+        instance=g.instance,
+        plugin="dashboard",
+        config=config,
+        dashboard=dashboard,
+        history=history or "",
+        vitejs=current_app.vitejs,
+    )
