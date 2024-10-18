3613484c7b7f2c6360dd04d7a16fc0abb52ce9a3
Étienne BERSAC <etienne.bersac@dalibo.com>
2024-10-18 09:31:06 +0200

    Version 9.0rc8

--- agent/temboardagent/version.py
+++ agent/temboardagent/version.py
@@ -1,1 +1,1 @@
-__version__ = "9.0rc7"
+__version__ = "9.0rc8"
--- ui/temboardui/version.py
+++ ui/temboardui/version.py
@@ -1,7 +1,7 @@
 import sys
 from platform import python_version
 
-__version__ = "9.0rc7"
+__version__ = "9.0rc8"
 
 
 # This output is parsed by tests/conftest.py::pytest_report_header.
