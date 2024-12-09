3dee195c4b444a7982b6215d623518913a08933d
Étienne BERSAC <etienne.bersac@dalibo.com>
2024-12-09 10:23:21 +0100

    Accept existing index on checks

--- ui/temboardui/model/versions/013_alerting-index.sql
+++ ui/temboardui/model/versions/013_alerting-index.sql
@@ -1,3 +1,3 @@
 SET search_path TO monitoring, public;
-CREATE INDEX idx_state_changes_key
+CREATE INDEX IF NOT EXISTS idx_state_changes_key
           ON monitoring.state_changes (check_id, key, datetime DESC);
